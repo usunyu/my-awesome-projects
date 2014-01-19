57b2625387fea6e5f0c050a6dd919b19b135f867
usunyu <usunyu@gmail.com>
2014-01-19 15:50:14 -0800

    course 5

--- src/main/java/com/spring/sun/test/App.java
+++ src/main/java/com/spring/sun/test/App.java
@@ -1,10 +1,15 @@
 package com.spring.sun.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
 		
+		Person person = (Person)context.getBean("person");
+		person.speak();
 	}
 
 }
