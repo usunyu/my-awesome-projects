bab3b7c7f7285cdb8a9ac8c3ef9c3e196cbc01a2
usunyu <usunyu@gmail.com>
2014-01-19 15:56:16 -0800

    course 6

--- src/main/java/com/spring/sun/test/App.java
+++ src/main/java/com/spring/sun/test/App.java
@@ -1,15 +1,18 @@
 package com.spring.sun.test;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/sun/test/beans/beans.xml");
 		
 		Person person = (Person)context.getBean("person");
 		person.speak();
+		
+		((ClassPathXmlApplicationContext)context).close();
 	}
 
 }
