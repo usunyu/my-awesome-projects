67f7e79f8ccce0943e3b6205c23bf74954d7a001
usunyu <usunyu@gmail.com>
2014-01-19 15:43:34 -0800

    initial submit

---
+++ src/main/java/com/spring/sun/test/App.java
@@ -0,0 +1,10 @@
+package com.spring.sun.test;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/spring/sun/test/Person.java
@@ -0,0 +1,7 @@
+package com.spring.sun.test;
+
+public class Person {
+	public void speak() {
+		System.out.println("Hello, I am a person!");
+	}
+}
