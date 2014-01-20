79bd7781d4ec0e4fb9000dcd724dbb77a8d45917
usunyu <usunyu@gmail.com>
2014-01-19 21:37:10 -0800

    Course 13: THE P NAMESPACE

--- src/main/java/com/spring/sun/test/Address.java
+++ src/main/java/com/spring/sun/test/Address.java
@@ -4,6 +4,10 @@ public class Address {
 	private String street;
 	private String postcode;
 	
+	public Address() {
+		
+	}
+	
 	public Address(String street, String postcode) {
 		super();
 		this.street = street;
@@ -11,11 +15,27 @@ public class Address {
 	}
 	
 	public void init() {
-		System.out.println("Bean created:" + this);
+		//System.out.println("Bean created:" + this);
 	}
 
 	@Override
 	public String toString() {
 		return "Address [street=" + street + ", postcode=" + postcode + "]";
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
 	}
 }
--- src/main/java/com/spring/sun/test/App.java
+++ src/main/java/com/spring/sun/test/App.java
@@ -15,6 +15,9 @@ public class App {
 		
 		System.out.println(person);
 		
+		Address address2 = (Address)context.getBean("address2");
+		System.out.println(address2);
+		
 		((ClassPathXmlApplicationContext)context).close();
 	}
 
