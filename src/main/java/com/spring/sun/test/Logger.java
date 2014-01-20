505d8b463fb3d67153f7979586b320472ca0b5fa
usunyu <usunyu@gmail.com>
2014-01-20 14:59:59 -0800

    Course 25: THE "AUTOWIRED" ANNOTATION

--- src/main/java/com/spring/sun/test/Logger.java
+++ src/main/java/com/spring/sun/test/Logger.java
@@ -1,18 +1,30 @@
 package com.spring.sun.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 /*
  * Dummy implementation of logger.
  */
 
 public class Logger {
 
+	//@Autowired
 	private ConsoleWriter consoleWriter;
+	//@Autowired
 	private FileWriter fileWriter;
+	
+	@Autowired
+	public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter) {
+		this.consoleWriter = consoleWriter;
+		this.fileWriter = fileWriter;
+	}
 
+	//@Autowired
 	public void setConsoleWriter(ConsoleWriter writer) {
 		this.consoleWriter = writer;
 	}
 
+	//@Autowired
 	public void setFileWriter(FileWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}
