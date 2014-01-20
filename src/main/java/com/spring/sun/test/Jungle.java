06d1937ab894dfd17bc5b2af6926a381ff487226
usunyu <usunyu@gmail.com>
2014-01-20 11:21:57 -0800

    Course 17: PROPERTY MAPS

--- src/main/java/com/spring/sun/test/Jungle.java
+++ src/main/java/com/spring/sun/test/Jungle.java
@@ -1,38 +1,28 @@
 package com.spring.sun.test;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Jungle {
-	private Animal largest;
-	private List<Animal> animals;
-
-	public Animal getLargest() {
-		return largest;
+	
+	private Map<String, String> foods = new HashMap<String, String>();
+	private Map<String, Animal> animals = new HashMap<String, Animal>();
+	
+	public void setFoods(Map<String, String> foods) {
+		this.foods = foods;
 	}
 
-	public void setLargest(Animal largest) {
-		this.largest = largest;
-	}
-
-	public List<Animal> getAnimals() {
-		return animals;
-	}
-
-	public void setAnimals(List<Animal> animals) {
+	public void setAnimals(Map<String, Animal> animals) {
 		this.animals = animals;
 	}
 
 	@Override
 	public String toString() {
+		
 		StringBuilder sb = new StringBuilder();
 		
-		sb.append("Largest: " + largest);
-		sb.append("\n");
-		sb.append("Others:\n");
-		
-		for(Animal animal: animals) {
-			sb.append(animal);
-			sb.append("\n");
+		for(Map.Entry<String, String> entry: foods.entrySet()) {
+			sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
 		}
 		
 		return sb.toString();
