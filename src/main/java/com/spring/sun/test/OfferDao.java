f5a2a3a567efca51b29c165b3f4b9b8994477e3f
usunyu <usunyu@gmail.com>
2014-01-23 17:51:47 -0800

    Course 41: JDBC TEMPLATES

--- src/main/java/com/spring/sun/test/OfferDao.java
+++ src/main/java/com/spring/sun/test/OfferDao.java
@@ -2,7 +2,22 @@ package com.spring.sun.test;
 
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OfferDao {
+	
+	private JdbcTemplate jdbc;
+	
+	@Autowired
+	public void setDataSource(DataSource jdbc) {
+		this.jdbc = new JdbcTemplate(jdbc);
+	}
+
 	public List<Offer> getOffers() {
 		return null;
 	}
