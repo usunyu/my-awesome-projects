2d822b3c61437686a276c5c013d06b95b4b33c47
usunyu <usunyu@gmail.com>
2014-01-23 16:30:00 -0800

    Course 38: IMPLEMENTING THE DAO PATTERN

---
+++ src/main/java/com/spring/sun/test/Offer.java
@@ -0,0 +1,39 @@
+package com.spring.sun.test;
+
+public class Offer {
+	private int id;
+	private String name;
+	private String email;
+	private String text;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	@Override
+	public String toString() {
+		return "Offer [id=" + id + ", name=" + name + ", email=" + email
+				+ ", text=" + text + "]";
+	}
+}
---
+++ src/main/java/com/spring/sun/test/OfferDao.java
@@ -0,0 +1,9 @@
+package com.spring.sun.test;
+
+import java.util.List;
+
+public class OfferDao {
+	public List<Offer> getOffers() {
+		return null;
+	}
+}
