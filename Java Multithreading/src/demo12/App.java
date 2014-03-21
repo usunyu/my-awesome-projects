2a5210930cec92044f27512fee06b5116e83676c
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-20 23:14:52 -0700

    Lec 10: Re-entrant Locks

---
+++ Java Multithreading/src/demo12/App.java
@@ -0,0 +1,42 @@
+package demo12;
+
+
+public class App {
+
+	public static void main(String[] args) throws InterruptedException {
+		final Runner runner = new Runner();
+		
+		Thread t1= new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					runner.firstThread();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		Thread t2= new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					runner.secondThread();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
+		
+		runner.finished();
+	}
+
+}
