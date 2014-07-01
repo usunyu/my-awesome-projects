29e4a5ff40803e9daa29de2a514dbddfb27ad9b7
usunyu <usunyu@gmail.com>
2014-06-30 18:03:34 -0700

    Swift: Simple Values

---
+++ The Swift Programming Language/A Swift Tour/SimpleValues.playground/section-1.swift
@@ -0,0 +1,52 @@
+var myVariable = 42
+
+myVariable = 50
+
+let myConstant = 42
+
+let implicitInteger = 70
+
+let implicitDouble = 70.0
+
+let explicitDouble: Double = 70
+
+let explicitFloat: Float = 40
+
+let label = "The width is "
+
+let width = 94
+
+let widthLabel = label + String(width)
+
+let apples = 3
+
+let oranges = 5
+
+let appleSummary = "I have \(apples) apples."
+
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+var name = "Chris"
+
+var floatCal = "Greeting " + name + ", \(explicitFloat + explicitFloat)"
+
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+
+shoppingList[1] = "bottle of water"
+
+shoppingList[1]
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+
+occupations["Jayne"] = "Public Relations"
+
+occupations["Jayne"]
+
+let emptyArray = String[]()
+
+let emptyDictionary = Dictionary<String, Float>()
+
+shoppingList = []   // Went shopping and bought everything.
