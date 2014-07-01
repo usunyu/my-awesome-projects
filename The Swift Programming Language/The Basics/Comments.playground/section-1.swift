1cdb193889fb448559156eb68a6c0e513657b3b9
usunyu <usunyu@gmail.com>
2014-07-01 00:46:02 -0700

    Swift: Comments

---
+++ The Swift Programming Language/The Basics/Comments.playground/section-1.swift
@@ -0,0 +1,8 @@
+// this is a comment
+
+/* this is also a comment,
+but written over multiple lines */
+
+/* this is the start of the first multiline comment
+/* this is the second, nested multiline comment */
+this is the end of the first multiline comment */
