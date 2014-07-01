38dfc6129b6c73e5fa5663c02909ca8250ef9a46
usunyu <usunyu@gmail.com>
2014-07-01 00:39:46 -0700

    Swift: “Naming Constants and Variables”

---
+++ The Swift Programming Language/The Basics/NamingConstantsAndVariables.playground/section-1.swift
@@ -0,0 +1,11 @@
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+// friendlyWelcome is now "Bonjour!"
+
+let languageName = "Swift"
+// languageName = "Swift++"
+// this is a compile-time error - languageName cannot be changed
