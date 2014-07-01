30c911b5b183a9030d461d8997c3dc8c5cd43862
usunyu <usunyu@gmail.com>
2014-07-01 00:44:42 -0700

    Swift: Printing Constants and Variables

---
+++ The Swift Programming Language/The Basics/PrintingConstantsAndVariables.playground/section-1.swift
@@ -0,0 +1,11 @@
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+
+println(friendlyWelcome)
+// prints "Bonjour!"
+
+println("This is a string")
+// prints "This is a string"
+
+println("The current value of friendlyWelcome is \(friendlyWelcome)")
+// prints "The current value of friendlyWelcome is Bonjour!"
