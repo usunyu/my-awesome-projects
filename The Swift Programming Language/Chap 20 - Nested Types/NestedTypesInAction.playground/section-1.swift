6b9af465bbea703ea5e8733007b174278c1a8071
usunyu <usunyu@gmail.com>
2014-07-05 15:04:11 -0700

    Swift: Chap 20 finish

---
+++ The Swift Programming Language/Chap 20 - Nested Types/NestedTypesInAction.playground/section-1.swift
@@ -0,0 +1,43 @@
+struct BlackjackCard {
+    // nested Suit enumeration
+    enum Suit: Character {
+        case Spades = "♠", Hearts = "♡", Diamonds = "♢", Clubs = "♣"
+    }
+    
+    // nested Rank enumeration
+    enum Rank: Int {
+        case Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+        case Jack, Queen, King, Ace
+        struct Values {
+            let first: Int, second: Int?
+        }
+        var values: Values {
+        switch self {
+        case .Ace:
+            return Values(first: 1, second: 11)
+        case .Jack, .Queen, .King:
+            return Values(first: 10, second: nil)
+        default:
+            return Values(first: self.toRaw(), second: nil)
+            }
+        }
+    }
+    
+    // BlackjackCard properties and methods
+    let rank: Rank, suit: Suit
+    var description: String {
+    var output = "suit is \(suit.toRaw()),"
+        output += " value is \(rank.values.first)"
+        if let second = rank.values.second {
+            output += " or \(second)"
+        }
+        return output
+    }
+}
+
+let theAceOfSpades = BlackjackCard(rank: .Ace, suit: .Spades)
+println("theAceOfSpades: \(theAceOfSpades.description)")
+// prints "theAceOfSpades: suit is ♠, value is 1 or 11"
+
+let heartsSymbol = BlackjackCard.Suit.Hearts.toRaw()
+// heartsSymbol is "♡"
