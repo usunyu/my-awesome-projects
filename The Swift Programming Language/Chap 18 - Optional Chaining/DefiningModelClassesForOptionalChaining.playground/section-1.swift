5f3effabbe651c483fd1d6a0bf11dae515c4db83
usunyu <usunyu@gmail.com>
2014-07-05 00:17:02 -0700

    Swift: Chap 18 pass

---
+++ The Swift Programming Language/Chap 18 - Optional Chaining/DefiningModelClassesForOptionalChaining.playground/section-1.swift
@@ -0,0 +1,37 @@
+class Person {
+    var residence: Residence?
+}
+
+class Residence {
+    var rooms = Room[]()
+    var numberOfRooms: Int {
+    return rooms.count
+    }
+    subscript(i: Int) -> Room {
+        return rooms[i]
+    }
+    func printNumberOfRooms() {
+        println("The number of rooms is \(numberOfRooms)")
+    }
+    var address: Address?
+}
+
+class Room {
+    let name: String
+    init(name: String) { self.name = name }
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    func buildingIdentifier() -> String? {
+        if buildingName {
+            return buildingName
+        } else if buildingNumber {
+            return buildingNumber
+        } else {
+            return nil
+        }
+    }
+}
---
+++ The Swift Programming Language/Chap 18 - Optional Chaining/OptionalChainingAsAnAlternativeToForcedUnwrapping.playground/section-1.swift
@@ -0,0 +1,28 @@
+class Person {
+    var residence: Residence?
+}
+
+class Residence {
+    var numberOfRooms = 1
+}
+
+let john = Person()
+
+//let roomCount = john.residence!.numberOfRooms
+// this triggers a runtime error
+
+if let roomCount = john.residence?.numberOfRooms {
+    println("John's residence has \(roomCount) room(s).")
+} else {
+    println("Unable to retrieve the number of rooms.")
+}
+// prints "Unable to retrieve the number of rooms."
+
+john.residence = Residence()
+
+if let roomCount = john.residence?.numberOfRooms {
+    println("John's residence has \(roomCount) room(s).")
+} else {
+    println("Unable to retrieve the number of rooms.")
+}
+// prints "John's residence has 1 room(s).
