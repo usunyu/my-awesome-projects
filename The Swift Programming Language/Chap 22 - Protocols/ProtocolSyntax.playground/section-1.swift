0a38138a88a1a587053ce260e9228b47a91e6faf
usunyu <usunyu@gmail.com>
2014-07-05 18:25:46 -0700

    Swift: Chap 22 finish

---
+++ The Swift Programming Language/Chap 22 - Protocols/ProtocolSyntax.playground/section-1.swift
@@ -0,0 +1,11 @@
+protocol SomeProtocol {
+    // protocol definition goes here
+}
+
+struct SomeStructure: FirstProtocol, AnotherProtocol {
+    // structure definition goes here
+}
+
+class SomeClass: SomeSuperclass, FirstProtocol, AnotherProtocol {
+    // class definition goes here
+}
