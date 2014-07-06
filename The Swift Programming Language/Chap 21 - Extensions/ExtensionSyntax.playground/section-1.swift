fbc4da87e13f2ddecae65809691f6d586eeb67af
usunyu <usunyu@gmail.com>
2014-07-05 18:09:55 -0700

    Swift: Chap 21 finish

---
+++ The Swift Programming Language/Chap 21 - Extensions/ExtensionSyntax.playground/section-1.swift
@@ -0,0 +1,7 @@
+extension SomeType {
+    // new functionality to add to SomeType goes here
+}
+
+extension SomeType: SomeProtocol, AnotherProtocol {
+    // implementation of protocol requirements goes here
+}
