a0271e199b64ceaa611d2046adb4afb1e9aa747f
usunyu <usunyu@gmail.com>
2014-07-07 15:44:17 -0700

    Swift: TipCalculator finish

--- Swift Application Tutorial/Ray Wenderlich/TipCalculator/TipCalculator/ViewController.swift
+++ Swift Application Tutorial/Ray Wenderlich/TipCalculator/TipCalculator/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    @IBOutlet var totalTextField : UITextField
+    @IBOutlet var taxPctSlider : UISlider
+    @IBOutlet var taxPctLabel : UILabel
+    @IBOutlet var resultsTextView : UITextView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calculateTapped(sender : AnyObject) {
+        // 1
+        tipCalc.total = Double(totalTextField.text.bridgeToObjectiveC().doubleValue)
+        // 2
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        // 3
+        for (tipPct, tipValue) in possibleTips {
+            // 4
+            results += "\(tipPct)%: \(tipValue)\n"
+        }
+        // 5
+        resultsTextView.text = results
+    }
+    @IBAction func taxPercentageChanged(sender : AnyObject) {
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
+    }
+    @IBAction func viewTapped(sender : AnyObject) {
+        totalTextField.resignFirstResponder()
+    }
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI() {
+        // 1
+        totalTextField.text = String(tipCalc.total)
+        // 2
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        // 3
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        // 4
+        resultsTextView.text = ""
+    }
 }
+
 
