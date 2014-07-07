e8ec322848f1c55d600d3a52ce5f42a91cf57ba7
usunyu <usunyu@gmail.com>
2014-07-07 12:30:53 -0700

    Swift: TipCalculator initial

---
+++ Swift Application Tutorial/Ray Wenderlich/TipCalculator/TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Sun on 7/7/14.
+//  Copyright (c) 2014 Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- Swift Application Tutorial/Ray Wenderlich/TipCalculator/TipCalculatorTests/TipCalculatorTests.swift
+++ Swift Application Tutorial/Ray Wenderlich/TipCalculator/TipCalculatorTests/TipCalculatorTests.swift
@@ -2,8 +2,8 @@
 //  TipCalculatorTests.swift
 //  TipCalculatorTests
 //
-//  Created by Main Account on 6/14/14.
-//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//  Created by Sun on 7/7/14.
+//  Copyright (c) 2014 Sun. All rights reserved.
 //
 
 import XCTest
