cba10a85ce9db213aac2972b8967e596aeec07c2
usunyu <usunyu@gmail.com>
2014-07-10 01:27:11 -0700

    iOS and Swift Beginner Tutorial: App like Twitter using Parse Part 1

---
+++ Swift Application Tutorial/Brian Advent/SwifferApp/SwifferApp/SweetTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  SweetTableViewCell.swift
+//  SwifferApp
+//
+//  Created by Sun on 7/10/14.
+//  Copyright (c) 2014 Sun. All rights reserved.
+//
+
+import UIKit
+
+class SweetTableViewCell: UITableViewCell {
+
+    init(style: UITableViewCellStyle, reuseIdentifier: String) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        // Initialization code
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
