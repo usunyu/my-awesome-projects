f0759cd6286be8f9e833fb8500eb9b8af7ad4219
usunyu <usunyu@gmail.com>
2014-07-30 01:41:31 -0700

    iOS and Swift Beginner Tutorial: Remote Push Notifications

--- Swift Application Tutorial/Brian Advent/SwifferApp/SwifferApp/AppDelegate.swift
+++ Swift Application Tutorial/Brian Advent/SwifferApp/SwifferApp/AppDelegate.swift
@@ -21,6 +21,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         Parse.setApplicationId("wPrMzXGaRIy6aGtHybUdKeiEQpBJmMG2tpo8XFl2", clientKey: "77R2zyO7JYWAcJ9qJrZLydCrouK2bJ1nkf40qp1o")
         
+        let notificationTypes:UIUserNotificationType = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound
+        let notificationSettings:UIUserNotificationSettings = UIUserNotificationSettings(forTypes: notificationTypes, categories: nil)
+        
+        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+        
         /*
         var testObject:PFObject = PFObject(className: "TestObject")
         testObject["foo"] = "bar"
@@ -31,7 +36,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(application: UIApplication!, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings!) {
+        UIApplication.sharedApplication().registerForRemoteNotifications()
+    }
+    
+    func application(application: UIApplication!, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData!) {
+        let currentInstallation:PFInstallation = PFInstallation.currentInstallation()
+        currentInstallation.setDeviceTokenFromData(deviceToken)
+        currentInstallation.saveInBackground()
+    }
+    
+    func application(application: UIApplication!, didFailToRegisterForRemoteNotificationsWithError error: NSError!) {
+        println(error.localizedDescription)
+    }
 
+    func application(application: UIApplication!, didReceiveRemoteNotification userInfo: NSDictionary!) {
+        var notification:NSDictionary = userInfo.objectForKey("aps") as NSDictionary
+        
+        if notification.objectForKey("content-available") {
+            if notification.objectForKey("content-available").isEqualToNumber(1) {
+                NSNotificationCenter.defaultCenter().postNotificationName("reloadTimeline", object: nil)
+            }
+        } else {
+                PFPush.handlePush(userInfo)
+        }
+    }
+    
     func applicationWillResignActive(application: UIApplication!) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
