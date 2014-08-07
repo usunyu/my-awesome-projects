6a8a822e60871285a7615f6373929e254d5c665c
Yu Sun <yusun@fb.com>
2014-08-06 19:14:36 -0700

    The Complete iOS 7 Age of Laika

---
+++ The Complete iOS 7/Age of Laika/Age of LaikaTests/Age_of_LaikaTests.m
@@ -0,0 +1,34 @@
+//
+//  Age_of_LaikaTests.m
+//  Age of LaikaTests
+//
+//  Created by Yu Sun on 8/6/14.
+//  Copyright (c) 2014 sunny. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Age_of_LaikaTests : XCTestCase
+
+@end
+
+@implementation Age_of_LaikaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
