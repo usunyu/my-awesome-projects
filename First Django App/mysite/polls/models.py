bce1a3c51ce0a13e4f648e289a28e9247bc2d1a7
Yu Sun <usunyu@gmail.com>
2014-12-13 23:22:51 -0800

    Writing your first Django app, part 2

--- First Django App/mysite/polls/models.py
+++ First Django App/mysite/polls/models.py
@@ -13,6 +13,9 @@ class Question(models.Model):
 
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
