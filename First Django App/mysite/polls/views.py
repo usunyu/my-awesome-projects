f4295082059bf077a3c7fb61dbff36b782f50700
Yu Sun <usunyu@gmail.com>
2014-12-15 00:03:27 -0800

    Writing your first Django app, part 3

--- First Django App/mysite/polls/views.py
+++ First Django App/mysite/polls/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.http import Http404
+
+from polls.models import Question
 
 # Create your views here.
+def index(request):
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+	response = "You're looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
