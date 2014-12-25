65caa7b6d3313b9c3543cac9452804326e0a338c
Yu Sun <usunyu@gmail.com>
2014-12-25 12:20:28 -0800

    Learn Python Django, Template Language
    Former-commit-id: 2ac7533f47b8227c91bc537ea3bd97210e01fdb8

--- Learn Python Django From Scratch/django-sun/django_test/django_test/settings.py
+++ Learn Python Django From Scratch/django-sun/django_test/django_test/settings.py
@@ -86,3 +86,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Customizing your project's templates
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
