063084235e50be48b861e6c59ed368e785d6afac
Yu Sun <usunyu@gmail.com>
2014-12-25 00:13:46 -0800

    Learn Python Django, Advanced Views and URLs
    Former-commit-id: 9ff9e6cf19324589cffc536418d7cfebe02999b4

--- Learn Python Django From Scratch/django-sun/django_test/article/views.py
+++ Learn Python Django From Scratch/django-sun/django_test/article/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+
+from article.models import Article
 
 # Create your views here.
+def articles(request):
+	context = {'articles': Article.objects.all()}
+	return render(request, 'article/articles.html', context)
+
+def article(request, article_id=1):
+	context = {'article': Article.objects.get(id=article_id)}
+	return render(request, 'article/article.html', context)
+
+# test demo
+def hello(request):
+	name = "Yu Sun"
+	html = "<html><body>Hi %s, this seems to have worked!</body></html>" % name
+	return HttpResponse(html)
+
+def hello_template(request):
+	name = "Yu Sun"
+	context = {'name': name}
+	t = get_template('article/hello.html')
+	html = t.render(Context(context))
+	return HttpResponse(html)
+
+def hello_template_simple(request):
+	name = "Yu Sun"
+	context = {'name': name}
+	return render(request, 'article/hello.html', context)
--- Learn Python Django From Scratch/django-sun/django_test/django_test/urls.py
+++ Learn Python Django From Scratch/django-sun/django_test/django_test/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^articles/', include('article.urls')),
 )
