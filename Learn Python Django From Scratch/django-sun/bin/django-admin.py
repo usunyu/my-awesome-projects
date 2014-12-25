431bc065a32e5d3cc57b09f5965af7b0f7c3d831
Yu Sun <usunyu@gmail.com>
2014-12-24 16:30:18 -0800

    Learn Python Django, Installation
    Former-commit-id: ff59a0ac84f81a9afbe93dc7ffc058c1d8eb865d

---
+++ Learn Python Django From Scratch/django-sun/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# EASY-INSTALL-SCRIPT: 'Django==1.7.1','django-admin.py'
+__requires__ = 'Django==1.7.1'
+import pkg_resources
+pkg_resources.run_script('Django==1.7.1', 'django-admin.py')
