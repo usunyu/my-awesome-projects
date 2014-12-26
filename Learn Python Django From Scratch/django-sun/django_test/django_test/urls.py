f4f31fa51dba87763c458e44a8e08808adfb5708
Yu Sun <usunyu@gmail.com>
2014-12-25 18:36:10 -0800

    Learn Python Django, Login and Logout
    Former-commit-id: 20db7ba51b7845e88320c30d9bc0f5be6b6cbe60

--- Learn Python Django From Scratch/django-sun/django_test/django_test/urls.py
+++ Learn Python Django From Scratch/django-sun/django_test/django_test/urls.py
@@ -8,4 +8,11 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^articles/', include('article.urls')),
+
+    # user auth urls
+    url(r'^accounts/login/$', 'django_test.views.login'),
+    url(r'^accounts/auth/$', 'django_test.views.auth_view'),
+    url(r'^accounts/logout/$', 'django_test.views.logout'),
+    url(r'^accounts/loggedin/$', 'django_test.views.loggedin'),
+    url(r'^accounts/invalid/$', 'django_test.views.invalid_login'),
 )
