4690bd3fd6e70255fdc809054a09c4be019f29f4
Yu Sun <usunyu@gmail.com>
2014-12-27 15:16:50 -0800

    Learn Python Django, Creating Forms for Database Models
    Former-commit-id: 02301e083d411b860833c587e64114f8d509fa68

--- Learn Python Django From Scratch/django-sun/django_test/article/models.py
+++ Learn Python Django From Scratch/django-sun/django_test/article/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
 	title = models.CharField(max_length=200)
 	body = models.TextField()
 	pub_date = models.DateTimeField('date published')
-	likes = models.IntegerField()
+	likes = models.IntegerField(default=0)
 
 	def __str__(self):	# __unicode__ on Python 2
 		return self.title
--- Learn Python Django From Scratch/django-sun/django_test/article/views.py
+++ Learn Python Django From Scratch/django-sun/django_test/article/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.context_processors import csrf
 from django.template.loader import get_template
 from django.template import Context
-
+from forms import ArticleForm
 from article.models import Article
 
 # Create your views here.
@@ -21,17 +22,31 @@ def articles(request):
 		'language': language,
 		'session_language': session_language
 	}
-	return render(request, 'article/articles.html', context)
+	return render(request, 'articles.html', context)
 
 def article(request, article_id=1):
 	context = {'article': Article.objects.get(id=article_id)}
-	return render(request, 'article/article.html', context)
+	return render(request, 'article.html', context)
 
 def language(request, language='en-us'):
 	response = HttpResponse("setting language to %s" % language)
 	response.set_cookie('lang', language)
 	request.session['lang'] = language
 	return response
+
+def create(request):
+	if request.POST:
+		form = ArticleForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/articles/all/')
+	else:
+		form = ArticleForm()
+
+	context = {}
+	context.update(csrf(request))
+	context['form'] = form
+	return render(request, 'create_article.html', context)
 
 # test demo
 def hello(request):
@@ -42,11 +57,11 @@ def hello(request):
 def hello_template(request):
 	name = "Yu Sun"
 	context = {'name': name}
-	t = get_template('article/hello.html')
+	t = get_template('hello.html')
 	html = t.render(Context(context))
 	return HttpResponse(html)
 
 def hello_template_simple(request):
 	name = "Yu Sun"
 	context = {'name': name}
-	return render(request, 'article/hello.html', context)
+	return render(request, 'hello.html', context)
