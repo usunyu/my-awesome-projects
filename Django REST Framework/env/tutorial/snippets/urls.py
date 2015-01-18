f0c16ef4a7e963ef34c0998d5fd67bc1b1328bec
Yu Sun <usunyu@gmail.com>
2015-01-18 15:39:41 -0800

    Django REST Framework, Tutorial 2: Requests and Responses

--- Django REST Framework/env/tutorial/snippets/urls.py
+++ Django REST Framework/env/tutorial/snippets/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
