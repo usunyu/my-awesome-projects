e5aa4c77b3e4f3e800a6129bfc3f10cf67c9fa93
Yu Sun <usunyu@gmail.com>
2015-01-18 18:33:38 -0800

    Django REST Framework, Tutorial 4: Authentication & Permissions

--- Django REST Framework/env/tutorial/snippets/serializers.py
+++ Django REST Framework/env/tutorial/snippets/serializers.py
@@ -1,9 +1,18 @@
 from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
-
+from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'owner', 'title', 'code', 'linenos', 'language', 'style')
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
--- Django REST Framework/env/tutorial/tutorial/urls.py
+++ Django REST Framework/env/tutorial/tutorial/urls.py
@@ -9,3 +9,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('snippets.urls')),
 )
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
