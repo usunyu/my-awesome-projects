7285ddb34bb26803e9713f550382f3ab009a938b
Yu Sun <usunyu@gmail.com>
2015-01-18 23:56:24 -0800

    Django REST Framework, Tutorial 5: Relationships & Hyperlinked APIs
    Former-commit-id: f1d47c7f2e61dc664cf0335072a6db14efdffa33

--- Django REST Framework/env/tutorial/snippets/urls.py
+++ Django REST Framework/env/tutorial/snippets/urls.py
@@ -1,13 +1,29 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
-urlpatterns = [
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
-    
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-]
+# API endpoints
+urlpatterns = format_suffix_patterns([
+    url(r'^$', views.api_root),
+    url(r'^snippets/$',
+        views.SnippetList.as_view(),
+        name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    url(r'^users/$',
+        views.UserList.as_view(),
+        name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='user-detail')
+])
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# Login and logout views for the browsable API
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
--- Django REST Framework/env/tutorial/tutorial/settings.py
+++ Django REST Framework/env/tutorial/tutorial/settings.py
@@ -83,3 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'PAGINATE_BY': 10
+}
