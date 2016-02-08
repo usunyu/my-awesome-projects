13634b4f2d05c84e764c303c942a9cb80021ae50
Yu Sun <usunyu@gmail.com>
2016-02-07 16:39:34 -0800

    [unity][lec 190] working around nested prefabs
    Former-commit-id: 294d78a94eb093bb8c3253b221fd45c1026f6722

---
+++ Unity/Learn To Code by Making Games Unity/Bowlmaster/Assets/Scripts/Pin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+
+	public float standingThreshold = 3f;
+
+	public bool IsStanding() {
+		Vector3 rotationInEuler = transform.rotation.eulerAngles;
+		float tiltInX = rotationInEuler.x;
+		float tiltInZ = rotationInEuler.z;
+
+		if (tiltInX < standingThreshold && tiltInZ < standingThreshold) {
+			return true;
+		}
+
+		return false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		IsStanding ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
