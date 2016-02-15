61cdb5ce0318846ea5461cab45f2ba25c55a8697
Yu Sun <usunyu@gmail.com>
2016-02-15 14:50:30 -0800

    [unity][lec 197] some debugging tips

--- Unity/Learn To Code by Making Games Unity/Bowlmaster/Assets/Scripts/Pin.cs
+++ Unity/Learn To Code by Making Games Unity/Bowlmaster/Assets/Scripts/Pin.cs
@@ -5,6 +5,9 @@ public class Pin : MonoBehaviour {
 
 	public float standingThreshold = 3f;
 
+	private float distanceToRaise = 40f;
+	private Rigidbody rigidbody;
+
 	public bool IsStanding() {
 		// hope the ball will be destoried :)
 		return true;
@@ -21,10 +24,26 @@ public class Pin : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 //		print (name + " " + IsStanding ());
+	}
+
+	public void Raise() {
+		if (IsStanding ()) {
+			rigidbody.useGravity = false;
+			rigidbody.velocity = Vector3.zero;
+			transform.Translate (new Vector3 (0, distanceToRaise, 0));
+		}
+	}
+
+	public void Lower() {
+		if (IsStanding ()) {
+			rigidbody.useGravity = true;
+			transform.Translate (new Vector3 (0, -distanceToRaise, 0));
+		}
 	}
 }
