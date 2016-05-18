87cbd78a9bb20c465c85fbc6531cefb1c84a4d3a
Yu Sun <usunyu@gmail.com>
2016-05-18 00:38:08 -0700

    [cpp] init project

---
+++ Cpp/CppBeginnerTutorial/CppBeginnerTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppBeginnerTutorial
+//
+//  Created by Yu Sun on 5/18/16.
+//  Copyright © 2016 Yu Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
