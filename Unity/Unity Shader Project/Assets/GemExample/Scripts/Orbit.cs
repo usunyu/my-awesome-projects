500abf4f21f469f04a7f46d9c2b83aad7d882a4c
Yu Sun <usunyu@gmail.com>
2016-08-22 22:46:42 +0800

    [shader] import gem project

---
+++ Unity/Unity Shader Project/Assets/GemExample/Scripts/Orbit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour {
+	
+	public float degreesPerSecond = 10;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (Vector3.zero, Vector3.up, degreesPerSecond * Time.deltaTime);
+	}
+}
