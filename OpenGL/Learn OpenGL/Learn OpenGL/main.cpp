766c707722279c8b92b09cc337ed1b028c35acce
Yu Sun <usunyu@gmail.com>
2016-10-06 00:24:56 +0800

    [learn opengl] Creating a window

---
+++ OpenGL/Learn OpenGL/Learn OpenGL/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  Learn OpenGL
+//
+//  Created by Yu Sun on 10/6/16.
+//  Copyright © 2016 Yu Sun. All rights reserved.
+//
+
+#include <iostream>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
