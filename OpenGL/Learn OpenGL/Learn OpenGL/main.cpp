92bd77389202c5f7fe8d989595f79890f40243bd
Yu Sun <usunyu@gmail.com>
2016-10-10 21:23:25 +0800

    [learn opengl] Hello Triangle2
    Former-commit-id: 84ff66589f7031d5163539b085c5308bf85bf9bb

--- OpenGL/Learn OpenGL/Learn OpenGL/main.cpp
+++ OpenGL/Learn OpenGL/Learn OpenGL/main.cpp
@@ -115,19 +115,34 @@ int main()
     glDeleteShader(fragmentShader);
     
     // Set up vertex data (and buffer(s)) and attribute pointers
+//    GLfloat vertices[] = {
+//        -0.5f, -0.5f, 0.0f, // Left
+//        0.5f, -0.5f, 0.0f, // Right
+//        0.0f,  0.5f, 0.0f  // Top
+//    };
     GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f, // Left
-        0.5f, -0.5f, 0.0f, // Right
-        0.0f,  0.5f, 0.0f  // Top
+        0.5f,  0.5f, 0.0f,  // Top Right
+        0.5f, -0.5f, 0.0f,  // Bottom Right
+        -0.5f, -0.5f, 0.0f,  // Bottom Left
+        -0.5f,  0.5f, 0.0f   // Top Left
     };
-    GLuint VBO, VAO;
+    GLuint indices[] = {  // Note that we start from 0!
+        0, 1, 3,  // First Triangle
+        1, 2, 3   // Second Triangle
+    };
+    
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
@@ -135,6 +150,9 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
     
     glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+    
+    // Uncommenting this call will result in wireframe polygons.
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     
     // Game loop
     while (!glfwWindowShouldClose(window))
@@ -150,7 +168,8 @@ int main()
         // Draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+//        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
         
         // Swap the screen buffers
