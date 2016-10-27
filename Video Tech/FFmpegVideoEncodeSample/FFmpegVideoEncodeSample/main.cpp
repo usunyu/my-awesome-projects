ccf360b7f12c45b109526953666648b8fd7abf84
Yu Sun <usunyu@gmail.com>
2016-10-27 22:13:41 +0800

    [video tech] simplest video encoder
    Former-commit-id: 3fe1895e020f6b398f55de7cf543fed98e8b2b4d

---
+++ Video Tech/FFmpegVideoEncodeSample/FFmpegVideoEncodeSample/main.cpp
@@ -0,0 +1,225 @@
+//
+//  main.cpp
+//  FFmpegVideoEncodeSample
+//
+//  Created by Yu Sun on 10/24/16.
+//  Copyright © 2016 Yu Sun. All rights reserved.
+//
+
+/**
+ * 最简单的基于FFmpeg的视频编码器
+ * Simplest FFmpeg Video Encoder
+ *
+ * 雷霄骅 Lei Xiaohua
+ * leixiaohua1020@126.com
+ * 中国传媒大学/数字电视技术
+ * Communication University of China / Digital TV Technology
+ * http://blog.csdn.net/leixiaohua1020
+ *
+ * 本程序实现了YUV像素数据编码为视频码流（H264，MPEG2，VP8等等）。
+ * 是最简单的FFmpeg视频编码方面的教程。
+ * 通过学习本例子可以了解FFmpeg的编码流程。
+ * This software encode YUV420P data to H.264 bitstream.
+ * It's the simplest video encoding software based on FFmpeg.
+ * Suitable for beginner of FFmpeg
+ */
+
+#include <iostream>
+extern "C" {
+#include <libavutil/opt.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+}
+
+extern "C" int flush_encoder(AVFormatContext *fmt_ctx,unsigned int stream_index){
+    int ret;
+    int got_frame;
+    AVPacket enc_pkt;
+    if (!(fmt_ctx->streams[stream_index]->codec->codec->capabilities &
+          CODEC_CAP_DELAY))
+        return 0;
+    while (1) {
+        enc_pkt.data = NULL;
+        enc_pkt.size = 0;
+        av_init_packet(&enc_pkt);
+        ret = avcodec_encode_video2 (fmt_ctx->streams[stream_index]->codec, &enc_pkt,
+                                     NULL, &got_frame);
+        av_frame_free(NULL);
+        if (ret < 0)
+            break;
+        if (!got_frame){
+            ret=0;
+            break;
+        }
+        printf("Flush Encoder: Succeed to encode 1 frame!\tsize:%5d\n",enc_pkt.size);
+        /* mux encoded frame */
+        ret = av_write_frame(fmt_ctx, &enc_pkt);
+        if (ret < 0)
+            break;
+    }
+    return ret;
+}
+
+int main(int argc, const char * argv[]) {
+    AVFormatContext* pFormatCtx;
+    AVOutputFormat* fmt;
+    AVStream* video_st;
+    AVCodecContext* pCodecCtx;
+    AVCodec* pCodec;
+    AVPacket pkt;
+    uint8_t* picture_buf;
+    AVFrame* pFrame;
+    int picture_size;
+    int y_size;
+    int framecnt=0;
+    //FILE *in_file = fopen("src01_480x272.yuv", "rb");	//Input raw YUV data
+    FILE *in_file = fopen("../../ds_480x272.yuv", "rb");   //Input raw YUV data
+    std::cout<< "in_file: " << in_file << std::endl;
+    int in_w=480,in_h=272;                              //Input data's width and height
+    int framenum=100;                                   //Frames to encode
+    //const char* out_file = "src01.h264";              //Output Filepath
+    //const char* out_file = "src01.ts";
+    //const char* out_file = "src01.hevc";
+    const char* out_file = "../../ds.h264";
+    
+    av_register_all();
+    //Method1.
+    pFormatCtx = avformat_alloc_context();
+    //Guess Format
+    fmt = av_guess_format(NULL, out_file, NULL);
+    pFormatCtx->oformat = fmt;
+    
+    //Method 2.
+    //avformat_alloc_output_context2(&pFormatCtx, NULL, NULL, out_file);
+    //fmt = pFormatCtx->oformat;
+    
+    
+    //Open output URL
+    if (avio_open(&pFormatCtx->pb,out_file, AVIO_FLAG_READ_WRITE) < 0){
+        printf("Failed to open output file! \n");
+        return -1;
+    }
+    
+    video_st = avformat_new_stream(pFormatCtx, 0);
+    //video_st->time_base.num = 1;
+    //video_st->time_base.den = 25;
+    
+    if (video_st==NULL){
+        return -1;
+    }
+    //Param that must set
+    pCodecCtx = video_st->codec;
+    //pCodecCtx->codec_id =AV_CODEC_ID_HEVC;
+    pCodecCtx->codec_id = fmt->video_codec;
+    pCodecCtx->codec_type = AVMEDIA_TYPE_VIDEO;
+    pCodecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
+    pCodecCtx->width = in_w;
+    pCodecCtx->height = in_h;
+    pCodecCtx->bit_rate = 400000;
+    pCodecCtx->gop_size=250;
+    
+    pCodecCtx->time_base.num = 1;
+    pCodecCtx->time_base.den = 25;
+    
+    //H264
+    //pCodecCtx->me_range = 16;
+    //pCodecCtx->max_qdiff = 4;
+    //pCodecCtx->qcompress = 0.6;
+    pCodecCtx->qmin = 10;
+    pCodecCtx->qmax = 51;
+    
+    //Optional Param
+    pCodecCtx->max_b_frames=3;
+    
+    // Set Option
+    AVDictionary *param = 0;
+    //H.264
+    if(pCodecCtx->codec_id == AV_CODEC_ID_H264) {
+        av_dict_set(&param, "preset", "slow", 0);
+        av_dict_set(&param, "tune", "zerolatency", 0);
+        //av_dict_set(&param, "profile", "main", 0);
+    }
+    //H.265
+    if(pCodecCtx->codec_id == AV_CODEC_ID_H265){
+        av_dict_set(&param, "preset", "ultrafast", 0);
+        av_dict_set(&param, "tune", "zero-latency", 0);
+    }
+    
+    //Show some Information
+    av_dump_format(pFormatCtx, 0, out_file, 1);
+    
+    pCodec = avcodec_find_encoder(pCodecCtx->codec_id);
+    if (!pCodec){
+        printf("Can not find encoder! \n");
+        return -1;
+    }
+    if (avcodec_open2(pCodecCtx, pCodec,&param) < 0){
+        printf("Failed to open encoder! \n");
+        return -1;
+    }
+    
+    
+    pFrame = av_frame_alloc();
+    picture_size = avpicture_get_size(pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height);
+    picture_buf = (uint8_t *)av_malloc(picture_size);
+    avpicture_fill((AVPicture *)pFrame, picture_buf, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height);
+    
+    //Write File Header
+    avformat_write_header(pFormatCtx,NULL);
+    
+    av_new_packet(&pkt,picture_size);
+    
+    y_size = pCodecCtx->width * pCodecCtx->height;
+    
+    for (int i=0; i<framenum; i++){
+        //Read raw YUV data
+        if (fread(picture_buf, 1, y_size*3/2, in_file) <= 0){
+            printf("Failed to read raw data! \n");
+            return -1;
+        }else if(feof(in_file)){
+            break;
+        }
+        pFrame->data[0] = picture_buf;              // Y
+        pFrame->data[1] = picture_buf+ y_size;      // U
+        pFrame->data[2] = picture_buf+ y_size*5/4;  // V
+        //PTS
+        //pFrame->pts=i;
+        pFrame->pts=i*(video_st->time_base.den)/((video_st->time_base.num)*25);
+        int got_picture=0;
+        //Encode
+        int ret = avcodec_encode_video2(pCodecCtx, &pkt,pFrame, &got_picture);
+        if(ret < 0){
+            printf("Failed to encode! \n");
+            return -1;
+        }
+        if (got_picture==1){
+            printf("Succeed to encode frame: %5d\tsize:%5d\n",framecnt,pkt.size);
+            framecnt++;
+            pkt.stream_index = video_st->index;
+            ret = av_write_frame(pFormatCtx, &pkt);
+            av_free_packet(&pkt);
+        }
+    }
+    //Flush Encoder
+    int ret = flush_encoder(pFormatCtx,0);
+    if (ret < 0) {
+        printf("Flushing encoder failed\n");
+        return -1;
+    }
+    
+    //Write file trailer
+    av_write_trailer(pFormatCtx);
+    
+    //Clean
+    if (video_st){
+        avcodec_close(video_st->codec);
+        av_free(pFrame);
+        av_free(picture_buf);
+    }
+    avio_close(pFormatCtx->pb);
+    avformat_free_context(pFormatCtx);
+    
+    fclose(in_file);
+    
+    return 0;
+}
---
+++ Video Tech/FFmpegVideoEncodeSample/readme.txt
@@ -0,0 +1,26 @@
+Run with Xcode on macOS
+
+----------------------------
+
+最简单的基于FFmpeg的视频编码器
+Simplest FFmpeg Video Encoder
+
+雷霄骅 Lei Xiaohua
+leixiaohua1020@126.com
+中国传媒大学/数字电视技术
+Communication University of China / Digital TV Technology
+http://blog.csdn.net/leixiaohua1020
+ 
+ 
+本程序实现了YUV像素数据编码为视频码流（H.265，H264，MPEG2，VP8等等）。
+是最简单的FFmpeg视频编码方面的教程。
+它包含以下两个子项目：
+simplest_ffmpeg_video_encoder：最简单的基于FFmpeg的视频编码器。使用libavcodec和libavformat编码并且封装视频。
+simplest_ffmpeg_video_encoder_pure：最简单的基于FFmpeg的视频编码器-纯净版。仅使用libavcodec编码视频，不使用libavformat。
+
+
+This software encode YUV420P raw data to video bitstream (Such as H.264, H.265, VP8, MPEG2 etc).
+It's the simplest video encoding software based on FFmpeg. 
+It contains following project:
+simplest_ffmpeg_video_encoder: Simplest video encoder. It uses libavcodec to encode video and uses libavformat to add container format to the video bitstream.
+simplest_ffmpeg_video_encoder_pure: Pure video encoder. It only uses libavcodec to encode video (without libavformat).
