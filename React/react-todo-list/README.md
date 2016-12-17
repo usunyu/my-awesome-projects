0ecab50d77c71d83a2ecfc4b563611858d5f9690
Yu Sun <usunyu@gmail.com>
2016-12-18 01:52:32 +0800

    [react] Hot Module Reloading | React + Redux + Webpack (Part 4)
    Former-commit-id: 5956a21922d08ffedcb2693259ddfd5032274af4

--- React/react-todo-list/README.md
+++ React/react-todo-list/README.md
@@ -1,3 +1,13 @@
 # react-todo-list
 
 This is a simple todo list app built with React, Redux and Webpack. I built this app during my [tutorial series on YouTube](https://www.youtube.com/playlist?list=PLQDnxXqV213JJFtDaG0aE9vqvp6Wm7nBg).
+
+##### Webpack Build:
+```
+$ webpack --config webpack.config.js
+```
+
+##### Run Server:
+```
+$ npm run serve
+```
--- React/react-todo-list/webpack.config.js
+++ React/react-todo-list/webpack.config.js
@@ -1,18 +1,28 @@
+var webpack = require('webpack');
+
 module.exports = {
     devtool: 'inline-source-map',
-    entry: ['./client/client.js'],
+    entry: [
+        'webpack-hot-middleware/client',
+        './client/client.js'
+    ],
     output: {
-        path: './dist',
+        path: require('path').resolve('./dist'),
         filename: 'bundle.js',
         publicPath: '/',
     },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
     module: {
         loaders: [{
             test: /\.js$/,
             loader: 'babel-loader',
             exculde: /node_modules/,
             query: {
-                presets: ['react', 'es2015']
+                presets: ['react', 'es2015', 'react-hmre']
             }
         }],
     }
