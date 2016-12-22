5c768d9595b09f2f2e39481287fd1f4bb93c3eaf
Yu Sun <usunyu@gmail.com>
2016-12-23 00:20:25 +0800

    [react] Using State and Props | React + Redux + Webpack (Part 5)
    Former-commit-id: 2403737e9d7e08bcb8a4f4eee9f7500016f6132a

--- React/react-todo-list/components/App.js
+++ React/react-todo-list/components/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import TextInput from './TextInput'
 
 class App extends Component {
 
 	render() {
-		return <div>This is definitely a hot reload React app now!</div>
+		return (
+            <div className="app">
+                <h1>This is App component</h1>
+                <TextInput/>
+            </div>
+        )
 	}
 
 }
