9447cd6b7357e381f8457fae8f8cb5d3b64d0475
Yu Sun <usunyu@gmail.com>
2016-12-23 22:17:26 +0800

    [react] Why Flux? | React + Redux + Webpack (Part 6)
    Former-commit-id: 26bdc96cd677df3f95e4bbdab6e9eb61b39a4157

--- React/react-todo-list/components/TextDisplay.js
+++ React/react-todo-list/components/TextDisplay.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react'
 
 class TextDisplay extends Component {
 
+    handleClick() {
+        // console.log(this);
+        this.props.deleteLetter();
+    }
+
     render() {
-        return <div>I'm displaying text from my parent: {this.props.text}</div>
+        return (
+            <div>
+                <div>I'm displaying text from my parent: {this.props.text}</div>
+                <button onClick={this.handleClick.bind(this)}>delete one letter</button>
+            </div>
+        )
     }
 }
 
