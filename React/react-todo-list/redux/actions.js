b0595bd6a2e3a2b24ac31f4639761a8b8f41fdc5
Yu Sun <usunyu@gmail.com>
2017-02-10 20:09:05 +0800

    [react] Implementing Redux 2 | React + Redux + Webpack (Part 9)
    Former-commit-id: 1b83ed8e53d0c8cd0511c2f2a823d8352b8a72e4

--- React/react-todo-list/redux/actions.js
+++ React/react-todo-list/redux/actions.js
@@ -1,10 +1,26 @@
 let actions = {
+    
     addTodo: function(text) {
         return {
             type: 'ADD_TODO',
             text: text,
         }
-    }
+    },
+
+    completeTodo: function(id) {
+        return {
+            type: 'COMPLETE_TODO',
+            id: id,
+        }
+    },
+
+    deleteTodo: function(id) {
+        return {
+            type: 'DELETE_TODO',
+            id: id,
+        }
+    },
+
 }
 
 // store.dispatch(addTodo('some text'))
