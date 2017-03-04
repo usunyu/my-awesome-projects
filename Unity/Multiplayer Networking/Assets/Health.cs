b23139d7d13e74923146af785bd550ba9321a145
Yu Sun <usunyu@gmail.com>
2017-03-04 20:14:21 +0800

    [unity] Multiplayer Networking Sample

---
+++ Unity/Multiplayer Networking/Assets/Health.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Health : NetworkBehaviour {
+
+    public const int maxHealth = 100;
+
+    public bool destroyOnDeath;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public RectTransform healthBar;
+
+    public void TakeDamage(int amount) {
+        if (!isServer)
+            return;
+
+        currentHealth -= amount;
+        if (currentHealth <= 0) {
+            if (destroyOnDeath) {
+                Destroy(gameObject);
+            }
+            else {
+                currentHealth = maxHealth;
+
+                // called on the Server, will be invoked on the Clients
+                RpcRespawn();
+            }
+        }
+    }
+
+    void OnChangeHealth(int currentHealth) {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn() {
+        if (isLocalPlayer) {
+            // Set the player’s position to origin
+            transform.position = Vector3.zero;
+        }
+    }
+}
