e97f42fa936d8641294c807348490a3b5b965b3c
Yu Sun <usunyu@gmail.com>
2017-05-03 14:24:37 +0800

    [unity] UNET Transport Layer API
    Former-commit-id: 0a8215bbee8cc4b649c4447fd2c04cdacf328bce

---
+++ Unity/UNET Prototype/Assets/Scripts/Client.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Player {
+    public string playerName;
+    public GameObject avatar;
+    public int connectionID;
+}
+
+public class Client : MonoBehaviour {
+
+    private const int MAX_CONNECTION = 100;
+
+    private int port = 8088;
+
+    private int hostID;
+    private int webHostID;
+
+    private int reliableChannel;
+    private int unreliableChannel;
+
+    private int ourClientID;
+    private int connectionID;
+
+    private float connectionTime;
+    private bool isConnected = false;
+    private bool isStarted = false;
+    private byte error;
+
+    private string playerName;
+    public GameObject playerPrefab;
+    public Dictionary<int, Player> players = new Dictionary<int, Player>();
+
+    public void Connect() {
+        // Does the player have a name?
+        string pName = GameObject.Find("NameInput").GetComponent<InputField>().text;
+        if (pName == "") {
+            Debug.Log("You must enter a name!");
+            return;
+        }
+
+        playerName = pName;
+
+        NetworkTransport.Init();
+        ConnectionConfig config = new ConnectionConfig();
+
+        reliableChannel = config.AddChannel(QosType.Reliable);
+        unreliableChannel = config.AddChannel(QosType.Unreliable);
+
+        HostTopology topology = new HostTopology(config, MAX_CONNECTION);
+
+        hostID = NetworkTransport.AddHost(topology);
+        connectionID = NetworkTransport.Connect(hostID, "0.0.0.0", port, 0, out error);
+
+        connectionTime = Time.time;
+        isConnected = true;
+    }
+
+    private void Update() {
+        if (!isConnected)
+            return;
+
+        int recHostId;
+        int connectionId;
+        int channelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+
+        NetworkEventType recData = NetworkTransport.Receive(
+            out recHostId,
+            out connectionId,
+            out channelId,
+            recBuffer,
+            bufferSize,
+            out dataSize,
+            out error);
+
+        switch (recData) {
+            case NetworkEventType.DataEvent:
+                string msg = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+                Debug.Log("Receiving: " + msg);
+                string[] splitData = msg.Split('|');
+                switch (splitData[0]) {
+                    case "ASKNAME":
+                        OnAskName(splitData);
+                        break;
+                    case "CNN":
+                        SpawnPlayer(splitData[1], int.Parse(splitData[2]));
+                        break;
+                    case "DC":
+                        PlayerDisconnected(int.Parse(splitData[1]));
+                        break;
+                    case "ASKPOSITION":
+                        OnAskPosition(splitData);
+                        break;
+                    default:
+                        Debug.Log("Invalid message: " + msg);
+                        break;
+                }
+                break;
+        }
+    }
+
+    private void OnAskName(string[] data) {
+        // Set this client's ID
+        ourClientID = int.Parse(data[1]);
+
+        // Send our name to the server
+        Send("NAMEIS|" + playerName, reliableChannel);
+        // Create all the other players
+        for (int i = 2; i < data.Length - 1; i++) {
+            string[] d = data[i].Split('%');
+            SpawnPlayer(d[0], int.Parse(d[1]));
+        }
+    }
+
+    private void OnAskPosition(string[] data) {
+        if (!isStarted)
+            return;
+
+        // Update everyone else
+        for (int i = 1; i < data.Length - 1; i++) {
+            string[] d = data[i].Split('%');
+
+            // Prevent the server updating us
+            if (ourClientID != int.Parse(d[0])) {
+                Vector3 position = Vector3.zero;
+                position.x = float.Parse(d[1]);
+                position.y = float.Parse(d[2]);
+                players[int.Parse(d[0])].avatar.transform.position = position;
+            }
+        }
+
+        // Send our own position
+        Vector3 myPosition = players[ourClientID].avatar.transform.position;
+        string m = "MYPOSITION|" + myPosition.x.ToString() + '%' + myPosition.y.ToString();
+        Send(m, unreliableChannel);
+    }
+
+    private void SpawnPlayer(string playerName, int cnnID) {
+        GameObject go = Instantiate(playerPrefab) as GameObject;
+
+        // Is this ours?
+        if (cnnID == ourClientID) {
+            // Add mobility
+            go.AddComponent<PlayerMotor>();
+            if (GameObject.Find("Canvas") != null)
+                GameObject.Find("Canvas").SetActive(false);
+            isStarted = true;
+        }
+
+        Player p = new Player();
+        p.avatar = go;
+        p.playerName = playerName;
+        p.connectionID = cnnID;
+        p.avatar.GetComponentInChildren<TextMesh>().text = playerName;
+        Debug.Log("Add cnnID: " + cnnID);
+        players.Add(cnnID, p);
+    }
+
+    private void PlayerDisconnected(int cnnID) {
+        Destroy(players[cnnID].avatar);
+        players.Remove(cnnID);
+    }
+
+    private void Send(string message, int channelID) {
+        Debug.Log("Sending: " + message);
+        byte[] msg = Encoding.Unicode.GetBytes(message);
+        NetworkTransport.Send(hostID, connectionID, channelID, msg, message.Length * sizeof(char), out error);
+    }
+}
---
+++ Unity/UNET Prototype/Assets/Scripts/Server.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerClient {
+    public int connectionID;
+    public string playerName;
+
+    public Vector3 position;
+}
+
+public class Server : MonoBehaviour {
+
+    private const int MAX_CONNECTION = 100;
+
+    private int port = 8088;
+
+    private int hostID;
+    private int webHostID;
+
+    private int reliableChannel;
+    private int unreliableChannel;
+
+    private bool isStarted = false;
+    private byte error;
+
+    private List<ServerClient> clients = new List<ServerClient>();
+
+    private float lastMovementUpdate;
+    private float movementUpdateRate = 0.05f;
+
+    private void Start() {
+        Application.runInBackground = true;
+
+        NetworkTransport.Init();
+        ConnectionConfig config = new ConnectionConfig();
+
+        reliableChannel = config.AddChannel(QosType.Reliable);
+        unreliableChannel = config.AddChannel(QosType.Unreliable);
+
+        HostTopology topology = new HostTopology(config, MAX_CONNECTION);
+
+        hostID = NetworkTransport.AddHost(topology, port, null);
+        webHostID = NetworkTransport.AddWebsocketHost(topology, port, null);
+
+        isStarted = true;
+    }
+
+    void Update() {
+        if (!isStarted)
+            return;
+
+        int recHostId;
+        int connectionId;
+        int channelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+
+        NetworkEventType recData = NetworkTransport.Receive(
+            out recHostId,
+            out connectionId,
+            out channelId,
+            recBuffer,
+            bufferSize,
+            out dataSize,
+            out error);
+
+        switch (recData) {
+            case NetworkEventType.Nothing:         //1
+                break;
+            case NetworkEventType.ConnectEvent:    //2
+                Debug.Log("Player " + connectionId + " has connected");
+                OnConnection(connectionId);
+                break;
+            case NetworkEventType.DataEvent:       //3
+                string msg = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+                Debug.Log("Receiving from " + connectionId + " : " + msg);
+                string[] splitData = msg.Split('|');
+                switch (splitData[0]) {
+                    case "NAMEIS":
+                        OnNameIs(connectionId, splitData[1]);
+                        break;
+                    case "MYPOSITION":
+                        string[] d = splitData[1].Split('%');
+                        OnMyPosition(connectionId, float.Parse(d[0]), float.Parse(d[1]));
+                        break;
+                    case "DC":
+
+                        break;
+                    default:
+                        Debug.Log("Invalid message: " + msg);
+                        break;
+                }
+                break;
+            case NetworkEventType.DisconnectEvent: //4
+                Debug.Log("Player " + connectionId + " has disconnected");
+                OnDisconnection(connectionId);
+                break;
+        }
+
+        // Ask player for their position
+        if (Time.time - lastMovementUpdate > movementUpdateRate) {
+            lastMovementUpdate = Time.time;
+            string m = "ASKPOSITION|";
+            foreach (ServerClient sc in clients) {
+                m += (sc.connectionID.ToString() + '%' + sc.position.x.ToString() + '%' + sc.position.y.ToString() + '|');
+            }
+            m.Trim('|');
+            Send(m, unreliableChannel, clients);
+        }
+    }
+
+    private void OnConnection(int connID) {
+        // When the player joins the server, tell him his ID
+        // Request his name and send the name of all other players
+        string msg = "ASKNAME|" + connID + "|";
+        foreach (ServerClient sc in clients) {
+            msg += sc.playerName + '%' + sc.connectionID + '|';
+        }
+        msg.Trim('|');
+
+        // Add him to a list
+        ServerClient c = new ServerClient();
+        c.connectionID = connID;
+        c.playerName = "TEMP";
+        clients.Add(c);
+
+        // ASKNAME|3|DAVE%1|MICHAEL%2|TEMP%3
+        Send(msg, reliableChannel, connID);
+    }
+
+    private void OnDisconnection(int connID) {
+        // Remove this player from our client list
+        clients.Remove(clients.Find((x) => x.connectionID == connID));
+
+        // Tell everyone that somebody else has disconnected
+        Send("DC|" + connID, reliableChannel, clients);
+    }
+
+    private void OnNameIs(int cnnID, string playerName) {
+        // Link the name to connection ID
+        clients.Find(x => x.connectionID == cnnID).playerName = playerName;
+        // Tell everybody that a new player has connected
+        Send("CNN|" + playerName + '|' + cnnID, reliableChannel, clients);
+    }
+
+    private void OnMyPosition(int cnnID, float x, float y) {
+        clients.Find(obj => obj.connectionID == cnnID).position = new Vector3(x, y, 0);
+    }
+
+    private void Send(string message, int channelID, int connID) {
+        List<ServerClient> c = new List<ServerClient>();
+        c.Add(clients.Find(x => x.connectionID == connID));
+        Send(message, channelID, c);
+    }
+
+    private void Send(string message, int channelID, List<ServerClient> c) {
+        Debug.Log("Sending: " + message);
+        byte[] msg = Encoding.Unicode.GetBytes(message);
+        foreach (ServerClient sc in c) {
+            NetworkTransport.Send(hostID, sc.connectionID, channelID, msg, message.Length * sizeof(char), out error);
+
+        }
+    }
+}
