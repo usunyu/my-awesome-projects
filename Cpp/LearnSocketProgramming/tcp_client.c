d5a8e6fbaca214a23f6c629880fdc5d01066fab7
Yu Sun <usunyu@gmail.com>
2017-05-04 20:13:52 +0800

    [c] coding sockets in c
    Former-commit-id: 9af642f687826ac42ad7d19c1f62196a34a39506

--- Cpp/LearnSocketProgramming/tcp_client.c
+++ Cpp/LearnSocketProgramming/tcp_client.c
@@ -10,7 +10,19 @@ int main() {
 
     // create a socket
     int network_socket;
-    network_socket = socket(AF_INT, SOCK_STREAM, 0);
+    network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // specify an address for the socket
+    struct sockaddr_in server_adress;
+    server_adress.sin_family = AF_INET;
+    server_adress.sin_port = htons(9002);
+    server_adress.sin_addr.s_addr = INADDR_ANY;
+
+    int connection_status = connect(network_socket, (struct sockaddr *) &server_adress, sizeof(server_adress));
+    // check for error with the connection
+    if (connection_status == -1) {
+        printf("There was an error making a connection to the remote server.\n");
+    }
 
     return 0;
 }
