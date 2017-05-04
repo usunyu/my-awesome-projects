80c77c76510e687664884599c3b8a30774054317
Yu Sun <usunyu@gmail.com>
2017-05-04 20:43:44 +0800

    [c] data transfer over sockets

--- Cpp/LearnSocketProgramming/tcp_client.c
+++ Cpp/LearnSocketProgramming/tcp_client.c
@@ -21,8 +21,17 @@ int main() {
     int connection_status = connect(network_socket, (struct sockaddr *) &server_adress, sizeof(server_adress));
     // check for error with the connection
     if (connection_status == -1) {
-        printf("There was an error making a connection to the remote server.\n");
+        printf("There was an error making a connection to the remote socket.\n\n");
     }
 
+    // recieve data from the server
+    char server_response[256];
+    recv(network_socket, &server_response, sizeof(server_response), 0);
+
+    // print out the server's response
+    printf("The server sent the data: %s\n", server_response);
+
+    // and then close the socket
+    close(network_socket);
     return 0;
 }
