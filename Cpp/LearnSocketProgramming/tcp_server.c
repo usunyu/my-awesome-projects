092a376ba0ab94754334946d562f667ea8c55629
Yu Sun <usunyu@gmail.com>
2017-05-04 21:42:14 +0800

    [c] communicating on the same machine
    Former-commit-id: 4e2387b47dcaed6e7f86a5b444a26193a511a53e

--- Cpp/LearnSocketProgramming/tcp_server.c
+++ Cpp/LearnSocketProgramming/tcp_server.c
@@ -14,5 +14,25 @@ int main() {
     int server_socket;
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
+    // define the server address
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    // bind the socket to our specified IP and port
+    bind(server_socket, (struct sockaddr *) & server_address, sizeof(server_address));
+
+    listen(server_socket, 5);
+
+    int client_socket;
+    client_socket = accept(server_socket, NULL, NULL);
+
+    // send the message 
+    send(client_socket, server_message, sizeof(server_message), 0);
+
+    // close the socket
+    close(server_socket);
+
     return 0;
 }
