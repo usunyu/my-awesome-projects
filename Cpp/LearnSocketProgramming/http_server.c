f28b19b9903c1cb0fd69de04aa7c1d0a146962ce
Yu Sun <usunyu@gmail.com>
2017-05-04 22:39:34 +0800

    [c] building the http server
    Former-commit-id: 3d2a1145fc2167bbb39d50d1ea27c0c5f8d2d23e

--- Cpp/LearnSocketProgramming/http_server.c
+++ Cpp/LearnSocketProgramming/http_server.c
@@ -7,11 +7,25 @@
 
 int main() {
 
+    // open a file to serve
     FILE *html_data;
     html_data = fopen("index.html", "r");
 
     char response_data[1024];
     fgets(response_data, 1024, html_data);
+
+    char http_header[2048] = "HTTP/1.1 200 OK\r\n\n";
+    strcat(http_header, response_data);
+
+    // create a socket
+    int server_socket;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // define the address
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8001);
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
     return 0;
 }
