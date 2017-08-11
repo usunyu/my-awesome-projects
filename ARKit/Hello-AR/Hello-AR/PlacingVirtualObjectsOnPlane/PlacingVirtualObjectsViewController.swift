079408bb2e828429ea46cf9e12cf7b2b978a1901
Yu Sun <usunyu@gmail.com>
2017-08-11 15:49:09 +0800

    [ARKit] Placing Virtual Objects on Plane

---
+++ ARKit/Hello-AR/Hello-AR/PlacingVirtualObjectsOnPlane/PlacingVirtualObjectsViewController.swift
@@ -0,0 +1,122 @@
+//
+//  PlacingVirtualObjectsViewController.swift
+//  Hello-AR
+//
+//  Created by Yu Sun on 8/11/17.
+//  Copyright © 2017 usunyu. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class PlacingVirtualObjectsViewController: UIViewController, ARSCNViewDelegate {
+    
+    var sceneView: ARSCNView!
+    var planes = [OverlayPlane]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.sceneView = ARSCNView(frame: self.view.frame)
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        
+        self.view.addSubview(self.sceneView)
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        registerGestureRecognizers()
+    }
+    
+    private func registerGestureRecognizers() {
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(recognizer :UIGestureRecognizer) {
+        
+        let sceneView = recognizer.view as! ARSCNView
+        let touchLocation = recognizer.location(in: sceneView)
+        
+        let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        
+        if !hitTestResult.isEmpty {
+            
+            guard let hitResult = hitTestResult.first else {
+                return
+            }
+            addBox(hitResult :hitResult)
+        }
+    }
+    
+    private func addBox(hitResult :ARHitTestResult) {
+        
+        let boxGeometry = SCNBox(width: 0.2, height: 0.2, length: 0.1, chamferRadius: 0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        boxGeometry.materials = [material]
+        
+        let boxNode = SCNNode(geometry: boxGeometry)
+        
+        boxNode.position = SCNVector3(hitResult.worldTransform.columns.3.x,hitResult.worldTransform.columns.3.y + Float(boxGeometry.height/2), hitResult.worldTransform.columns.3.z)
+        
+          self.sceneView.scene.rootNode.addChildNode(boxNode)
+        
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = .horizontal
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        if !(anchor is ARPlaneAnchor) {
+            return
+        }
+        
+        let plane = OverlayPlane(anchor: anchor as! ARPlaneAnchor)
+        self.planes.append(plane)
+        node.addChildNode(plane)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        let plane = self.planes.filter { plane in
+            return plane.anchor.identifier == anchor.identifier
+            }.first
+        
+        if plane == nil {
+            return
+        }
+        
+        plane?.update(anchor: anchor as! ARPlaneAnchor)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
