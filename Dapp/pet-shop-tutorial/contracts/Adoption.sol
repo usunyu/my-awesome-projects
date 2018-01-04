0291cc8634f4311b6cd90e9aef0a228757fd16fe
Yu Sun <usunyu@gmail.com>
2018-01-04 12:09:43 +0800

    [Dapp] Ethereum Pet Shop, Update

--- Dapp/pet-shop-tutorial/contracts/Adoption.sol
+++ Dapp/pet-shop-tutorial/contracts/Adoption.sol
@@ -1,19 +1,19 @@
 pragma solidity ^0.4.17;
 
 contract Adoption {
-    address[16] public adopters;
+  address[16] public adopters;
 
-    // Adopting a pet
-    function adopt(uint petId) public view returns (uint) {
-        require(petId >= 0 && petId <= 15);
+  // Adopting a pet
+  function adopt(uint petId) public returns (uint) {
+    require(petId >= 0 && petId <= 15);
 
-        adopters[petId] = msg.sender;
+    adopters[petId] = msg.sender;
 
-        return petId;
-    }
+    return petId;
+  }
 
-    // Retrieving the adopters
-    function getAdopters() public returns (address[16]) {
-        return adopters;
-    }
+  // Retrieving the adopters
+  function getAdopters() public view returns (address[16]) {
+    return adopters;
+  }
 }
--- Dapp/pet-shop-tutorial/test/TestAdoption.sol
+++ Dapp/pet-shop-tutorial/test/TestAdoption.sol
@@ -15,4 +15,25 @@ contract TestAdoption {
 
     Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
   }
+
+  // Testing retrieval of a single pet's owner
+  function testGetAdopterAddressByPetId() public {
+    // Expected owner is this contract
+    address expected = this;
+
+    address adopter = adoption.adopters(8);
+
+    Assert.equal(adopter, expected, "Owner of pet ID 8 should be recorded.");
+  }
+
+  // Testing retrieval of all pet owners
+  function testGetAdopterAddressByPetIdInArray() public {
+    // Expected owner is this contract
+    address expected = this;
+
+    // Store adopters in memory rather than contract's storage
+    address[16] memory adopters = adoption.getAdopters();
+
+    Assert.equal(adopters[8], expected, "Owner of pet ID 8 should be recorded.");
+  }
 }
