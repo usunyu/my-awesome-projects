12d098895160d0e8c89409b88ab908444ddcdd81
Yu Sun <usunyu@gmail.com>
2018-01-05 15:58:22 +0800

    [React] Modern React with Redux, 15 ~ 20
    Former-commit-id: bbbfdc3eea6c11942a8754a4e9b2c000e3cfe875

--- React/react-redux/src/components/search_bar.js
+++ React/react-redux/src/components/search_bar.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />;
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
+            </div>
+        );
+    }
 }
 
 export default SearchBar;
