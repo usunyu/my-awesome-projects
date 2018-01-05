96456f4ba0c7ebdc9666f68d89bfc70e218ba817
Yu Sun <usunyu@gmail.com>
2018-01-05 14:29:46 +0800

    [React] Modern React with Redux, 12 ~ 14

--- React/react-redux/src/index.js
+++ React/react-redux/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDG02yAnswI0fbFCxcT659snMsTYKbaeyU';
+
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
