91e8fe09d1e709df3893b4707ae80032b2bcdcf7
Yu Sun <usunyu@gmail.com>
2018-01-06 10:59:12 +0800

    [React] Modern React with Redux, 26 ~ 29

--- React/react-redux/src/index.js
+++ React/react-redux/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDG02yAnswI0fbFCxcT659snMsTYKbaeyU';
 
@@ -10,10 +11,16 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null,
+        };
 
         YTSearch({key: API_KEY, term: 'serfboards'}, (videos) => {
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0],
+            });
         });
     }
 
@@ -21,7 +28,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }
