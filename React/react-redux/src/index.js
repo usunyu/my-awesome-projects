fce2ed002054798621732206b136223d23794b0b
Yu Sun <usunyu@gmail.com>
2018-01-06 11:48:55 +0800

    [React] Modern React with Redux, 30 ~ 33

--- React/react-redux/src/index.js
+++ React/react-redux/src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -16,7 +17,11 @@ class App extends Component {
             selectedVideo: null,
         };
 
-        YTSearch({key: API_KEY, term: 'serfboards'}, (videos) => {
+        this.videoSearch('hello world');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0],
@@ -25,9 +30,11 @@ class App extends Component {
     }
 
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
