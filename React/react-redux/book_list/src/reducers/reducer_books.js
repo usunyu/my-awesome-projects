a80da5bec8801ab16b7942bd622e964a41f86f56
Yu Sun <usunyu@gmail.com>
2018-01-07 14:35:49 +0800

    [React] Modern React with Redux, 38 ~ 43

---
+++ React/react-redux/book_list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: The Good Parts' },
+        { title: 'Harry Potter' },
+        { title: 'The Dark Tower' },
+        { title: 'Ruby on Rails' },
+    ]
+}
