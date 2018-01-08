73f6064984d86504aa2a64b19450c1ff1e0367cc
Yu Sun <usunyu@gmail.com>
2018-01-08 21:37:04 +0800

    [React] Modern React with Redux, 122 ~ 128

---
+++ React/react-redux/blog/src/components/posts_index.js
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+class PostsIndex extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className='list-group-item' key={post.id}>
+                    {post.title}
+                </li>
+            )
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Posts</h3>
+                <ul className='list-group'>
+                    {this.renderPosts()}
+                </ul>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {posts: state.posts};
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
--- React/react-redux/blog/src/index.js
+++ React/react-redux/blog/src/index.js
@@ -3,31 +3,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-    render() {
-        return <div>Hello!</div>;
-    }
-}
-
-class Goodbye extends React.Component {
-    render() {
-        return <div>Goodbye!</div>;
-    }
-}
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
           <div>
-              Header
-              <Route path='/hello' component={Hello} />
-              <Route path='/goodbye' component={Goodbye} />
+              <Route path='/' component={PostsIndex} />
           </div>
       </BrowserRouter>
   </Provider>
