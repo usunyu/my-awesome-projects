f6e6f92ea3a88727ea29823ea926f2c6314c6ddb
Yu Sun <usunyu@gmail.com>
2018-01-09 03:34:30 +0800

    [React] Modern React with Redux, 129 ~ 141

--- React/react-redux/blog/src/index.js
+++ React/react-redux/blog/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -14,7 +15,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
           <div>
-              <Route path='/' component={PostsIndex} />
+              <Switch>
+                  <Route path='/posts/new' component={PostsNew} />
+                  <Route path='/' component={PostsIndex} />
+              </Switch>
           </div>
       </BrowserRouter>
   </Provider>
