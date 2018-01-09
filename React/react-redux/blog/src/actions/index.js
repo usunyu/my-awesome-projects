1de14ecbde97927a739dce07101a4bffdb4e5505
Yu Sun <usunyu@gmail.com>
2018-01-09 14:05:43 +0800

    [React] Modern React with Redux, 142 ~ 151
    Former-commit-id: 9a965836cb93acfb3ed7078f571c994552c1da2f

--- React/react-redux/blog/src/actions/index.js
+++ React/react-redux/blog/src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const FETCH_POST = 'FETCH_POST';
+export const CREATE_POST = 'CREATE_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=usunyu';
@@ -11,5 +14,36 @@ export function fetchPosts() {
     return {
         type: FETCH_POSTS,
         payload: request
+    };
+}
+
+export function createPost(values, callback) {
+    const request =
+        axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+        .then(() => callback());
+
+    return {
+        type: CREATE_POST,
+        payload: request
+    };
+}
+
+export function fetchPost(id) {
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+    return {
+        type: FETCH_POST,
+        payload: request
+    };
+}
+
+export function deletePost(id, callback) {
+    const request =
+        axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+        .then(() => callback());
+
+    return {
+        type: DELETE_POST,
+        payload: id
     };
 }
--- React/react-redux/blog/src/components/posts_new.js
+++ React/react-redux/blog/src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 // {...field.input}
 // =>
@@ -27,7 +29,9 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -70,4 +74,6 @@ function validate(values) {
 export default reduxForm({
     validate, // validate: validate
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, {createPost})(PostsNew)
+);
