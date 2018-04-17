b147b3b7085cbb34a906193254e1081f6f1d1d7a
Yu Sun <usunyu@gmail.com>
2018-04-18 00:45:45 +0800

    [Design Pattern] Design Patterns Video Tutorial 2
    Former-commit-id: 552059e7b005851145aba1734a72c813b97a8be0

---
+++ Design Pattern/Design Patterns in Java/Monkey.java
@@ -0,0 +1,62 @@
+public class Monkey implements Living {
+
+    @Override
+    public void setName(String newName) {
+
+    }
+    
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setHeight(double newHeight) {
+
+    }
+    
+    @Override
+    public double getHeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWeight(double newWeight) {
+
+    }
+    
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public void setFavFood(String newFood) {
+
+    }
+    
+    @Override
+    public String getFavFood() {
+        return null;
+    }
+
+    @Override
+    public void setSpeed(double newSpeed) {
+
+    }
+    
+    @Override
+    public double getSpeed() {
+        return 0;
+    }
+
+    @Override
+    public void setSound(String newSound) {
+
+    }
+    
+    @Override
+    public String getSound() {
+        return null;
+    }
+}
--- Design Pattern/Design Patterns in Java/WorkWithAnimals.java
+++ Design Pattern/Design Patterns in Java/WorkWithAnimals.java
@@ -13,10 +13,37 @@ public class WorkWithAnimals {
 
         changeObjectName(fido);
         System.out.println("Dog name after method call: " + fido.getName());
+
+        Animal doggy = new Dog();
+        Animal kitty = new Cat();
+
+        System.out.println("Doggy says: " + doggy.getSound());
+        System.out.println("Kitty says: " + kitty.getSound());
+
+        Animal[] animals = new Animal[4];
+        animals[0] = doggy;
+        animals[1] = kitty;
+
+        System.out.println("Doggy says: " + animals[0].getSound());
+        System.out.println("Kitty says: " + animals[1].getSound());
+
+        speakAnimal(doggy);
+
+        ((Dog)doggy).digHole();
+
+        fido.accessPrivate();
+
+        Giraffe giraffe = new Giraffe();
+        giraffe.setName("Frank");
+        System.out.println(giraffe.getName());
     }
 
     public static void changeObjectName(Dog fido) {
         fido.setName("Marcus");
 
+    }
+
+    public static void speakAnimal(Animal animal) {
+        System.out.println("Animal says: " + animal.getSound());
     }
 }
