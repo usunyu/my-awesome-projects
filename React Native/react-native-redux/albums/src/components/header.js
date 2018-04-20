10fc7df0bfe74170382bcc2e4aa7c4de440609c9
Yu Sun <usunyu@gmail.com>
2018-04-21 00:29:29 +0800

    [RN] Components & File Exports
    Former-commit-id: 314210b14118192519251336148d3cdbee4d0b08

---
+++ React Native/react-native-redux/albums/src/components/header.js
@@ -0,0 +1,9 @@
+// Import libraries for making component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a component
+const Header = () => <Text>Albums!</Text>;
+
+// Make the component available to other parts of the app
+export default Header;
