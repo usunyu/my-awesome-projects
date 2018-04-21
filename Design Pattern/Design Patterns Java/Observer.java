7ddf00fb576b9af62b03d985a0a05cf391657e3d
Yu Sun <usunyu@gmail.com>
2018-04-22 00:44:17 +0800

    [Design Pattern] Observer Design Pattern
    Former-commit-id: 5737a8f3dc54f75917f3cc6cdda6a365aa4a2ecd

---
+++ Design Pattern/Design Patterns Java/Observer.java
@@ -0,0 +1,3 @@
+public interface Observer {
+    public void update(double ibmPrice, double aapplPrice, double googPrice);
+}
---
+++ Design Pattern/Design Patterns Java/StockObserver.java
@@ -0,0 +1,37 @@
+public class StockObserver implements Observer {
+
+    private double ibmPrice;
+    private double aaplPrice;
+    private double googPrice;
+
+    private static int observerIDTracker = 0;
+
+    private int observerID;
+
+    private Subject stockGrabber;
+
+    public StockObserver(Subject stockGrabber) {
+
+        this.stockGrabber = stockGrabber;
+
+        this.observerID = ++observerIDTracker;
+
+        System.out.println("New Observer " + this.observerID);
+
+        stockGrabber.register(this);
+    }
+
+    @Override
+    public void update(double ibmPrice, double aaplPrice, double googPrice) {
+
+        this.ibmPrice = ibmPrice;
+        this.aaplPrice = aaplPrice;
+        this.googPrice = googPrice;
+
+        printThePrices();
+    }
+
+    public void printThePrices() {
+        System.out.println(observerID + "\nIBM: " + ibmPrice + "\nAAPL: " + aaplPrice + "\nGOOG: " + googPrice + "\n");
+    }
+}
