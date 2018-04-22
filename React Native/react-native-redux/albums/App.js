4ba24b88c33143d5714a266693fd978951eeb335
Yu Sun <usunyu@gmail.com>
2018-04-22 22:29:59 +0800

    [RN] Component props
    Former-commit-id: 71ec12ce253483cb50cbb224f4eb917dd014ed8a

--- React Native/react-native-redux/albums/App.js
+++ React Native/react-native-redux/albums/App.js
@@ -4,7 +4,7 @@ import Header from './src/components/header';
 export default class App extends React.Component {
     render() {
         return (
-            <Header />
+            <Header headerText={'Albums'} />
         );
     }
 }
