74685f61e604995154b0ce0f3b9360bd36db9752
Yu Sun <usunyu@gmail.com>
2018-04-22 20:16:33 +0800

    [Design Pattern] Factory Design Pattern
    Former-commit-id: 504e352d06fb3de10ebc4e135af591393a87e6ab

---
+++ Design Pattern/Design Patterns Java/RocketEnemyShip.java
@@ -0,0 +1,8 @@
+public class RocketEnemyShip extends EnemyShip {
+
+    public RocketEnemyShip() {
+
+        setName("Rocket Enemy Ship");
+        setDamage(10.0);
+    }
+}
---
+++ Design Pattern/Design Patterns Java/UFOEnemyShip.java
@@ -0,0 +1,8 @@
+public class UFOEnemyShip extends EnemyShip {
+
+    public UFOEnemyShip() {
+
+        setName("UFO Enemy Ship");
+        setDamage(20.0);
+    }
+}
