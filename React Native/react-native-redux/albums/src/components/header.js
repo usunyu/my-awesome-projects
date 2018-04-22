9946a0a1ce086a2832cfd0d19fcae11cc8fe0efa
Yu Sun <usunyu@gmail.com>
2018-04-22 18:02:45 +0800

    [RN] Component styling
    Former-commit-id: 2a5e0070c04ab360a144a71b18c992843a319d89

--- React Native/react-native-redux/albums/src/components/header.js
+++ React Native/react-native-redux/albums/src/components/header.js
@@ -1,9 +1,26 @@
 // Import libraries for making component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
-const Header = () => <Text>Albums!</Text>;
+const Header = () => {
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums!</Text>
+        </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+    },
+    textStyle: {
+        fontSize: 20,
+    },
+};
 
 // Make the component available to other parts of the app
 export default Header;
