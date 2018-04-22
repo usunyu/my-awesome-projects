71ec12ce253483cb50cbb224f4eb917dd014ed8a
Yu Sun <usunyu@gmail.com>
2018-04-22 22:29:59 +0800

    [RN] Component props

--- React Native/react-native-redux/albums/src/components/header.js
+++ React Native/react-native-redux/albums/src/components/header.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 // Make a component
-const Header = () => {
+const Header = (props) => {
     const { textStyle, viewStyle } = styles;
 
     return (
         <View style={viewStyle}>
-            <Text style={textStyle}>Albums!</Text>
+            <Text style={textStyle}>{props.headerText}</Text>
         </View>
     );
 };
@@ -16,6 +16,15 @@ const Header = () => {
 const styles = {
     viewStyle: {
         backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative',
     },
     textStyle: {
         fontSize: 20,
