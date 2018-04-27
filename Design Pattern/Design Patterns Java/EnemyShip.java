cea3b575869eb318b3187aea16cfce692639a742
Yu Sun <usunyu@gmail.com>
2018-04-28 01:16:31 +0800

    [Design Pattern] Abstract Factory Design Pattern
    Former-commit-id: e772e6fd55a6ae9537799d0a09bd561621978e1f

--- Design Pattern/Design Patterns Java/EnemyShip.java
+++ Design Pattern/Design Patterns Java/EnemyShip.java
@@ -1,7 +1,47 @@
+//public abstract class EnemyShip {
+//
+//    private String name;
+//    private double amtDamage;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String newName) {
+//        name = newName;
+//    }
+//
+//    public double getDamage() {
+//        return amtDamage;
+//    }
+//
+//    public void setDamage(double newDamage) {
+//        amtDamage = newDamage;
+//    }
+//
+//    public void displayEnemyShip(){
+//        System.out.println(getName() + " is on the screen");
+//    }
+//
+//    public void followHelloShip() {
+//        System.out.println(getName() + " is following the hero");
+//    }
+//
+//    public void enemyShipShoots() {
+//        System.out.println(getName() + " attacks and does " + getDamage());
+//    }
+//}
+
 public abstract class EnemyShip {
 
     private String name;
-    private double amtDamage;
+
+    // Newly defined objects that represent weapon & engine
+    // These can be changed easily by assigning new parts
+    // in UFOEnemyShipFactory or UFOBossEnemyShipFactory
+
+    ESWeapon weapon;
+    ESEngine engine;
 
     public String getName() {
         return name;
@@ -11,23 +51,39 @@ public abstract class EnemyShip {
         name = newName;
     }
 
-    public double getDamage() {
-        return amtDamage;
+    abstract void makeShip();
+
+    // Because I defined the toString method in engine
+    // when it is printed the String defined in toString goes
+    // on the screen
+
+    public void followHeroShip() {
+
+        System.out.println(getName() + " is following the hero at " + engine);
+
     }
 
-    public void setDamage(double newDamage) {
-        amtDamage = newDamage;
-    }
+    public void displayEnemyShip() {
 
-    public void displayEnemyShip(){
         System.out.println(getName() + " is on the screen");
-    }
 
-    public void followHelloShip() {
-        System.out.println(getName() + " is following the hero");
     }
 
     public void enemyShipShoots() {
-        System.out.println(getName() + " attacks and does " + getDamage());
+
+        System.out.println(getName() + " attacks and does " + weapon);
+
     }
+
+    // If any EnemyShip object is printed to screen this shows up
+
+    public String toString() {
+
+        String infoOnShip = "The " + name + " has a top speed of " + engine +
+                " and an attack power of " + weapon;
+
+        return infoOnShip;
+
+    }
+
 }
