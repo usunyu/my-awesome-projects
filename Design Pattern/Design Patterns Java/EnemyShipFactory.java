e772e6fd55a6ae9537799d0a09bd561621978e1f
Yu Sun <usunyu@gmail.com>
2018-04-28 01:16:31 +0800

    [Design Pattern] Abstract Factory Design Pattern

--- Design Pattern/Design Patterns Java/EnemyShipFactory.java
+++ Design Pattern/Design Patterns Java/EnemyShipFactory.java
@@ -1,18 +1,32 @@
-public class EnemyShipFactory {
+//public class EnemyShipFactory {
+//
+//    public EnemyShip makeEnemyShip(String newShipType) {
+//
+//        EnemyShip newShip = null;
+//
+//        if (newShipType.equals("U")) {
+//            return new UFOEnemyShip();
+//        }
+//        else if (newShipType.equals("R")) {
+//            return new RocketEnemyShip();
+//        }
+//        else if (newShipType.equals("B")) {
+//            return new BigUFOEnemyShip();
+//        }
+//        return null;
+//    }
+//}
 
-    public EnemyShip makeEnemyShip(String newShipType) {
+// With an Abstract Factory Pattern you won't
+// just build ships, but also all of the components
+// for the ships
 
-        EnemyShip newShip = null;
+// Here is where you define the parts that are required
+// if an object wants to be an enemy ship
 
-        if (newShipType.equals("U")) {
-            return new UFOEnemyShip();
-        }
-        else if (newShipType.equals("R")) {
-            return new RocketEnemyShip();
-        }
-        else if (newShipType.equals("B")) {
-            return new BigUFOEnemyShip();
-        }
-        return null;
-    }
+public interface EnemyShipFactory {
+
+    public ESWeapon addESGun();
+    public ESEngine addESEngine();
+
 }
--- Design Pattern/Design Patterns Java/UFOBossEnemyShip.java
+++ Design Pattern/Design Patterns Java/UFOBossEnemyShip.java
@@ -1,13 +1,4 @@
-//public class UFOEnemyShip extends EnemyShip {
-//
-//    public UFOEnemyShip() {
-//
-//        setName("UFO Enemy Ship");
-//        setDamage(20.0);
-//    }
-//}
-
-public class UFOEnemyShip extends EnemyShip {
+public class UFOBossEnemyShip extends EnemyShip {
 
     // We define the type of ship we want to create
     // by stating we want to use the factory that
@@ -19,24 +10,26 @@ public class UFOEnemyShip extends EnemyShip {
     // this method. They state that the enemy ship
     // must have a weapon and engine assigned. That
     // object also states the specific parts needed
-    // to make a regular UFO versus a Boss UFO
+    // to make a Boss UFO versus a Regular UFO
 
-    public UFOEnemyShip(EnemyShipFactory shipFactory) {
+    public UFOBossEnemyShip(EnemyShipFactory shipFactory) {
 
         this.shipFactory = shipFactory;
 
     }
 
     // EnemyShipBuilding calls this method to build a
-    // specific UFOEnemyShip
+    // specific UFOBossEnemyShip
 
     void makeShip() {
+
+        // TODO Auto-generated method stub
 
         System.out.println("Making enemy ship " + getName());
 
         // The specific weapon & engine needed were passed in
         // shipFactory. We are assigning those specific part
-        // objects to the UFOEnemyShip here
+        // objects to the UFOBossEnemyShip here
 
         weapon = shipFactory.addESGun();
         engine = shipFactory.addESEngine();
