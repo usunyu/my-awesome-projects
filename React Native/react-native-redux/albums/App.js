ed47985f3d5b7c0200d9c8ef9ed10d629cfa5004
Yu Sun <usunyu@gmail.com>
2018-04-30 17:20:44 +0800

    [RN] HTTP Requests with React Native
    Former-commit-id: 581fc4d14f92b24ba8aeaf67b7c683a7c690f7fe

--- React Native/react-native-redux/albums/App.js
+++ React Native/react-native-redux/albums/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList'
 
 export default class App extends React.Component {
     render() {
         return (
-            <Header headerText={'Albums'} />
+            <View>
+                <Header headerText={'Albums'} />
+                <AlbumList />
+            </View>
         );
     }
 }
