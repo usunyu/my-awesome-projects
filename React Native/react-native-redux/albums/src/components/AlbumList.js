581fc4d14f92b24ba8aeaf67b7c683a7c690f7fe
Yu Sun <usunyu@gmail.com>
2018-04-30 17:20:44 +0800

    [RN] HTTP Requests with React Native

---
+++ React Native/react-native-redux/albums/src/components/AlbumList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+// const AlbumList = () => {
+class AlbumList extends Component {
+    state = { albums: [] };
+
+    componentDidMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => {
+                this.setState({ albums: response.data });
+            });
+    }
+
+    renderAlbums() {
+        return this.state.albums.map((album) =>
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
+    render() {
+        console.log(this.state);
+
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
