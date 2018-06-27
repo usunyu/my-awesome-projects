953086a8dbc5e0859b8781c05d4999fd940d9da2
Yu Sun <usunyu@gmail.com>
2018-06-27 12:56:41 +0800

    [CryptoZombies] Lesson 2: Zombies Attack Their Victims

--- Dapp/crypto-zombies/contracts/zombiefactory.sol
+++ Dapp/crypto-zombies/contracts/zombiefactory.sol
@@ -17,8 +17,10 @@ contract ZombieFactory {
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string _name, uint _dna) private {
+    function _createZombie(string _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
     }
 
@@ -28,7 +30,9 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
