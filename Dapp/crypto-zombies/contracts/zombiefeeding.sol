e0780ba74e3e113bd9dbcbb11d7cf73c7be8e632
Yu Sun <usunyu@gmail.com>
2018-09-06 17:30:10 +0800

    [CryptoZombies] Lesson 3: Advanced Solidity Concepts, Part 2
    Former-commit-id: 3d3f6484e6d47a52249a5c6c7f8496f49ede9d5a

--- Dapp/crypto-zombies/contracts/zombiefeeding.sol
+++ Dapp/crypto-zombies/contracts/zombiefeeding.sol
@@ -25,15 +25,25 @@ contract ZombieFeeding is ZombieFactory {
     kittyContract = KittyInterface(_address);
   }
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+      return (_zombie.readyTime <= now);
+  }
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
+    require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
     if (keccak256(_species) == keccak256("kitty")) {
       newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
+    _triggerCooldown(myZombie);
   }
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
