4188685c206adbf55e7645a7ab00edef05b079ef
Yu Sun <usunyu@gmail.com>
2019-02-26 19:33:16 -0800

    [Flutter] HTTP Requests with Flutter, I
    Former-commit-id: 1cb70e4ffef60bc67abe9b1647ddebdf4e02b08e

--- Flutter/pics/lib/src/app.dart
+++ Flutter/pics/lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
 
 class App extends StatefulWidget {
   createState() {
@@ -9,17 +10,18 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   int counter = 0;
 
+  void fetchImage() {
+    counter += 1;
+    get('https://jsonplaceholder.typicode.com/photos/$counter');
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
+          onPressed: fetchImage,
         ),
         appBar: AppBar(
           title: Text("Let's see some images!"),
