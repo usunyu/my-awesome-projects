534eaef1e1bfe461144d79061962f46a5be44e76
Yu Sun <usunyu@gmail.com>
2019-02-26 18:06:22 -0800

    [Flutter] State Design with Flutter Apps

--- Flutter/pics/lib/main.dart
+++ Flutter/pics/lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('Hello worlds!');
-        },
-      ),
-      appBar: AppBar(
-        title: Text("Let's see some images!"),
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }
--- Flutter/pics/lib/src/app.dart
+++ Flutter/pics/lib/src/app.dart
@@ -1,1 +1,30 @@
 import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('$counter'),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
+        ),
+        appBar: AppBar(
+          title: Text("Let's see some images!"),
+        ),
+      ),
+    );
+  }
+}
