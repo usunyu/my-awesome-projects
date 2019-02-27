6997f9a93c3abdd29aec1eb65bdfc5e359fa4ace
Yu Sun <usunyu@gmail.com>
2019-02-27 13:16:53 -0800

    [Flutter] Forms and Validation with Flutter

---
+++ Flutter/login_stateful/lib/src/mixins/validation_mixin.dart
@@ -0,0 +1,15 @@
+class ValidationMixin {
+  String validateEmail(String value) {
+    if (!value.contains('@')) {
+      return 'Please enter a valid email!';
+    }
+    return null;
+  }
+
+  String validatePassword(String value) {
+    if (value.length < 4) {
+      return 'Password must at least 4 characters!';
+    }
+    return null;
+  }
+}
---
+++ Flutter/login_stateful/lib/src/screens/login_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import '../mixins/validation_mixin.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> with ValidationMixin {
+  final formKey = GlobalKey<FormState>();
+
+  String email = '';
+  String password = '';
+
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        key: formKey,
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            Container(margin: EdgeInsets.only(bottom: 25.0)),
+            passwordField(),
+            Container(margin: EdgeInsets.only(bottom: 25.0)),
+            submitButton(),
+          ],
+        )
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: "Email Address",
+        hintText: "you@example.com",
+      ),
+      validator: validateEmail,
+      onSaved: (String value) {
+        email = value;
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "Password",
+        hintText: "your password here",
+      ),
+      obscureText: true,
+      validator: validatePassword,
+      onSaved: (String value) {
+        password = value;
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text("Submit"),
+      onPressed: () {
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print('Success with $email, $password');
+        }
+      },
+    );
+  }
+}
