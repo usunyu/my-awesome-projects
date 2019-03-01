c9eb851e82f82c7667e8e77ab7ffbb45018ef442
Yu Sun <usunyu@gmail.com>
2019-02-28 23:52:17 -0800

    [Flutter] Consuming BLOC Data, Scoped Blocs

--- Flutter/login_bloc/lib/src/screens/login_screen.dart
+++ Flutter/login_bloc/lib/src/screens/login_screen.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   Widget build(context) {
+    final bloc = Provider.of(context);
     return Container(
         margin: EdgeInsets.all(20.0),
         child: Column(
           children: <Widget>[
-            emailField(),
-            passwordField(),
+            emailField(bloc),
+            passwordField(bloc),
             Container(margin: EdgeInsets.only(top: 25.0)),
             submitButton(),
           ],
         ));
   }
 
-  Widget emailField() {
+  Widget emailField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.email,
       builder: (context, snapshot) {
@@ -34,7 +36,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget passwordField() {
+  Widget passwordField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.password,
       builder: (context, snapshot) {
