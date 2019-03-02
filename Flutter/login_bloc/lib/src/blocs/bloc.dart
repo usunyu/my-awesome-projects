e50d53e4f89fb2b9c2d6ce04640970823641d930
Yu Sun <usunyu@gmail.com>
2019-03-01 22:02:05 -0800

    [Flutter] RxDart for Reactive Programming
    Former-commit-id: 340b0215b0098af7cce86dcc3f96e5d60872ca6e

--- Flutter/login_bloc/lib/src/blocs/bloc.dart
+++ Flutter/login_bloc/lib/src/blocs/bloc.dart
@@ -1,15 +1,26 @@
 import 'dart:async';
 import 'validator.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Bloc with Validator {
-  final _email = StreamController<String>();
-  final _password = StreamController<String>();
+  final _email = BehaviorSubject<String>();
+  final _password = BehaviorSubject<String>();
 
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
+  Stream<bool> get submitValid =>
+      Observable.combineLatest2(email, password, (e, p) => true);
 
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
+
+  submit() {
+    final validEmail = _email.value;
+    final validPassword =_password.value;
+
+    print('Email is $validEmail');
+    print('Password is $validPassword');
+  }
 
   dispose() {
     _email.close();
