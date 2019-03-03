b4c4aca427aead449c1eab0bd8b25c206d96fdfd
Yu Sun <usunyu@gmail.com>
2019-03-03 14:07:17 -0800

    [Flutter] Fetching Individual Items
    Former-commit-id: 266b56e269ca9326c22061b7e8fe037590100030

---
+++ Flutter/news/lib/src/resources/news_api_provider.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' show Client;
+import '../models/item_model.dart';
+
+final _root = 'https://hacker-news.firebaseio.com/v0';
+
+class NewsApiProvider {
+  Client client = new Client();
+
+  fetchTopIds() async {
+    final response = await client.get('$_root/topstories.json');
+    final ids =json.decode(response.body);
+
+    return ids;
+  }
+
+  fetchItem(int id) async {
+    final response = await client.get('$_root/item/$id.json');
+    final parsedJson = json.decode(response.body);
+
+    return ItemModel.fromJson(parsedJson);
+  }
+}
