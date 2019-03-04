4d4a38c2978ae1ed1d2ec822578dcfc4b5f02a50
Yu Sun <usunyu@gmail.com>
2019-03-03 17:04:53 -0800

    [Flutter] Offline Data Storage

--- Flutter/news/lib/src/models/item_model.dart
+++ Flutter/news/lib/src/models/item_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ItemModel {
   final int id;
   final bool deleted;
@@ -27,4 +29,37 @@ class ItemModel {
       score = parsedJson['score'],
       title = parsedJson['title'],
       descendants = parsedJson['descendants'];
+  
+  ItemModel.fromDb(Map<String, dynamic> parsedJson)
+    : id = parsedJson['id'],
+      deleted = parsedJson['deleted'] == 1,
+      type = parsedJson['type'],
+      by = parsedJson['by'],
+      time = parsedJson['time'],
+      text = parsedJson['text'],
+      dead = parsedJson['dead'] == 1,
+      parent = parsedJson['parent'],
+      kids = jsonDecode(parsedJson['kids']),
+      url = parsedJson['url'],
+      score = parsedJson['score'],
+      title = parsedJson['title'],
+      descendants = parsedJson['descendants'];
+
+  Map<String, dynamic> toMapForDb() {
+    return <String, dynamic> {
+      "id": id,
+      "deleted": deleted ? 1 : 0,
+      "type": type,
+      "by": by,
+      "time": time,
+      "text": text,
+      "dead": dead ? 1 : 0,
+      "parent": parent,
+      "kids": jsonEncode(kids),
+      "url": url,
+      "score": score,
+      "title": title,
+      "descendants": descendants
+    };
+  }
 }
---
+++ Flutter/news/lib/src/resources/news_db_provider.dart
@@ -0,0 +1,55 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../models/item_model.dart';
+
+class NewsDbProvider {
+  Database db;
+
+  void init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'items.db');
+
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+          CREATE TABEL Items
+          (
+            id INTEGER PRIMARY KEY,
+            type TEXT,
+            by TEXT,
+            time INTEGER,
+            parent INTEGER,
+            kids BLOB,
+            dead INTEGER,
+            deleted INTEGER,
+            url TEXT,
+            title TEXT,
+            descendants INTEGER
+          )
+        """);
+      },
+    );
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id=?",
+      whereArgs: [id],
+    );
+    if (maps.length > 0) {
+      return ItemModel.fromDb(maps.first);
+    }
+    return null;
+  }
+
+  Future<int> addItem(ItemModel item) {
+    return db.insert("Items", item.toMapForDb());
+  }
+}
