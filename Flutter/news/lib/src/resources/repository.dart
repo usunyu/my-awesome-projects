b297fd0156355eab6f7282173a366f1ade58d1f6
Yu Sun <usunyu@gmail.com>
2019-03-04 11:42:46 -0800

    [Flutter] Type Design
    Former-commit-id: 4243c93f1b4e8154d25c8d435cfb4e4f02fe6c35

--- Flutter/news/lib/src/resources/repository.dart
+++ Flutter/news/lib/src/resources/repository.dart
@@ -4,20 +4,40 @@ import 'news_db_provider.dart';
 import '../models/item_model.dart';
 
 class Repository {
-  NewsDbProvider dbProvider = NewsDbProvider();
-  NewsApiProvider apiProvider = NewsApiProvider();
+  List<Source> sources = <Source>[
+    newsDbProvider,
+    NewsApiProvider(),
+  ];
+  List<Cache> caches = <Cache> [
+    newsDbProvider,
+  ];
 
+  // TODO
   Future<List<int>> fetchTopIds() {
-    return apiProvider.fetchTopIds();
+    return sources[1].fetchTopIds();
   }
 
   Future<ItemModel> fetchItem(int id) async {
-    var item = await dbProvider.fetchItem(id);
-    if (item != null) {
-      return item;
+    ItemModel item;
+    Source source;
+    for (source in sources) {
+      item = await source.fetchItem(id);
+      if (item != null) {
+        break;
+      }
     }
-    item = await apiProvider.fetchItem(id);
-    dbProvider.addItem(item);
+    for (var cache in caches) {
+      cache.addItem(item);
+    }
     return item;
   }
+}
+
+abstract class Source {
+  Future<List<int>> fetchTopIds();
+  Future<ItemModel> fetchItem(int id);
+}
+
+abstract class Cache {
+  Future<int> addItem(ItemModel item);
 }
