ec62a64e760321848bab208b1f1c1d8076fb45e9
Yu Sun <usunyu@gmail.com>
2019-03-03 17:04:53 -0800

    [Flutter] Offline Data Storage
    Former-commit-id: 4d4a38c2978ae1ed1d2ec822578dcfc4b5f02a50

--- Flutter/news/lib/src/resources/news_api_provider.dart
+++ Flutter/news/lib/src/resources/news_api_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:async';
 import 'package:http/http.dart' show Client;
 import '../models/item_model.dart';
 
@@ -7,14 +8,14 @@ final _root = 'https://hacker-news.firebaseio.com/v0';
 class NewsApiProvider {
   Client client = new Client();
 
-  fetchTopIds() async {
+  Future<List<int>> fetchTopIds() async {
     final response = await client.get('$_root/topstories.json');
     final ids =json.decode(response.body);
 
-    return ids;
+    return ids.cast<int>();
   }
 
-  fetchItem(int id) async {
+  Future<ItemModel> fetchItem(int id) async {
     final response = await client.get('$_root/item/$id.json');
     final parsedJson = json.decode(response.body);
 
