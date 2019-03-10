3fe72a997849db2cc123751cff1efd5d34419fc8
Yu Sun <usunyu@gmail.com>
2019-03-10 15:53:22 -0700

    [React SSR] Lets Get Coding!
    Former-commit-id: 4199896858a50f9df56810a441e18255933b0254

---
+++ React/react-ssr/server/webpack.server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { target: { browsers: ['last 2 versions'] }}]
+          ]
+        }
+      }
+    ]
+  }
+};
