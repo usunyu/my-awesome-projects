2e46e3ef4cbc385b70930cf17e91569d897f0c66
Yu Sun <usunyu@gmail.com>
2019-04-16 20:16:44 -0700

    [React SSR] Integrating Support for Redux

---
+++ React/react-ssr/server/src/client/components/UserList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
+
+class UserList extends Component {
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUsers() {
+    return this.props.users.map(user => {
+      return <li key={user.id}>{user.name}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Here's big list of users:
+        <ul>{this.renderUsers()}</ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    users: state.users
+  };
+}
+
+export default connect(mapStateToProps, { fetchUsers })(UserList);
--- React/react-ssr/server/src/index.js
+++ React/react-ssr/server/src/index.js
@@ -1,11 +1,14 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
