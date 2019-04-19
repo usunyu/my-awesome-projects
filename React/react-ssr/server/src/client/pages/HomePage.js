24aa112a2644550bab8c3dd773a28ee300ea6154
Yu Sun <usunyu@gmail.com>
2019-04-18 20:04:02 -0700

    [React SSR] Organization with Page Components
    Former-commit-id: 5a34da1141868d7f4a36a9d30ce58f94c9f33378

--- React/react-ssr/server/src/client/pages/HomePage.js
+++ React/react-ssr/server/src/client/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = () => {
+const HomePage = () => {
   return (
     <div>
       <div>This is Home Page</div>
@@ -9,4 +9,6 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default {
+  component: HomePage,
+}
--- React/react-ssr/server/src/helpers/renderer.js
+++ React/react-ssr/server/src/helpers/renderer.js
@@ -3,6 +3,7 @@ import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
+import serialize from 'serialize-javascript';
 import Routes from '../client/Routes';
 
 export default (req, store) => {
@@ -19,6 +20,9 @@ export default (req, store) => {
       <head></head>
       <body>
         <div id="root">${content}</div>
+        <script>
+          window.INITIAL_STATE = ${serialize(store.getState())}
+        </script>
         <script src="bundle.js"></script>
       </body>
     </html>
