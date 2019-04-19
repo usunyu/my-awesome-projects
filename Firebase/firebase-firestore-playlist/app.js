7ea9623c317e58c29c2754c27409b3e946b8a61e
Yu Sun <usunyu@gmail.com>
2019-04-18 21:10:54 -0700

    [Firestore] Tutorial #4 - Saving Data
    Former-commit-id: 697da3f31c42abdcf2737ac42a5eac44c78def5c

--- Firebase/firebase-firestore-playlist/app.js
+++ Firebase/firebase-firestore-playlist/app.js
@@ -1,4 +1,5 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 // create element and render cafe
 function renderCafe(doc) {
@@ -16,8 +17,20 @@ function renderCafe(doc) {
   cafeList.appendChild(li);
 }
 
+// getting data
 db.collection('cafes').get().then((snapshot) => {
   snapshot.docs.forEach(doc => {
     renderCafe(doc);
   });
+});
+
+// saving data
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('cafes').add({
+    name: form.name.value,
+    city: form.city.value,
+  });
+  form.name.value = '';
+  form.city.value = '';
 });
