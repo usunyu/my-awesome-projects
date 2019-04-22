91bd6b793238eea85077b9deb4deaabdd929ba3f
Yu Sun <usunyu@gmail.com>
2019-04-21 21:36:32 -0700

    [NextJS] Create Dynamic Pages

--- React/next-learn/pages/index.js
+++ React/next-learn/pages/index.js
@@ -1,9 +1,23 @@
 import Layout from '../components/MyLayout.js';
+import Link from 'next/link';
 
-export default function Index() {
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
+
+export default function Blog() {
   return (
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
   );
 };
