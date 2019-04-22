3f3e42c1a182e09cde8dbdb7a977e5ba902cd089
Yu Sun <usunyu@gmail.com>
2019-04-22 16:11:46 -0700

    [NextJS] Styling Components
    Former-commit-id: 9a2e62794536856ba5ae5da1ac5dafa89523d7aa

--- React/next-learn/pages/index.js
+++ React/next-learn/pages/index.js
@@ -14,10 +14,34 @@ const Index = (props) => (
         </li>
       ))}
     </ul>
+    <style jsx>{`
+      h1,
+      a {
+        font-family: 'Arial';
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 );
 
-Index.getInitialProps = async function() {
+Index.getInitialProps = async function () {
   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
   const data = await res.json();
 
--- React/next-learn/pages/post.js
+++ React/next-learn/pages/post.js
@@ -1,12 +1,45 @@
-import { withRouter } from 'next/router';
 import Layout from '../components/MyLayout';
 import fetch from 'isomorphic-unfetch';
+import Markdown from 'react-markdown';
 
 const Post = props => (
   <Layout>
     <h1>{props.show.name}</h1>
     <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
     <img src={props.show.image.medium} />
+    <div className="markdown">
+      <Markdown
+        source={`
+This is our blog post.
+Yes. We can have a [link](/link).
+And we can have a title as well.
+
+### This is a title
+
+And here's the content.
+     `}
+      />
+    </div>
+    <style jsx global>{`
+      .markdown {
+        font-family: 'Arial';
+      }
+
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
 );
 
