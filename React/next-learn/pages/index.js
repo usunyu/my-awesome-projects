0b524da4101eec5b4a003129bc0a64dd4cf3dc2c
Yu Sun <usunyu@gmail.com>
2019-04-21 23:15:19 -0700

    [NextJS] Server Side Support for Clean URLs
    Former-commit-id: d820c3b720259f48220a6ba6668540d32a310710

--- React/next-learn/pages/index.js
+++ React/next-learn/pages/index.js
@@ -3,20 +3,20 @@ import Link from 'next/link';
 
 const PostLink = props => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </li>
-)
+);
 
 export default function Blog() {
   return (
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink title="Hello Next.js" />
-        <PostLink title="Learn Next.js is awesome" />
-        <PostLink title="Deploy apps with Zeit" />
+        <PostLink id="hello-nextjs" title="Hello Next.js" />
+        <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+        <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
       </ul>
     </Layout>
   );
