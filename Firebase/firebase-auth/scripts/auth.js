3241e24035de691ad0eac731f4bb6a288e87685f
Yu Sun <usunyu@gmail.com>
2019-05-03 18:23:04 -0700

    [Firebase] Auth Tutorial #7 - Logging Users In
    Former-commit-id: 97c1b075f88d7d561a7d5cc0478121818472215d

---
+++ Firebase/firebase-auth/scripts/auth.js
@@ -0,0 +1,43 @@
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // signup the user
+  auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user log out');
+  });
+});
+
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
