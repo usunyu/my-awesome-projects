9ebd33a4e3abc09432f80b7832f885a4d4d609f5
Yu Sun <usunyu@gmail.com>
2019-12-19 20:11:51 -0800

    [Gatsby] Introduction to Styling in Gatsby
    Former-commit-id: 6cf9b86e0fe118515624e472be2883ece5fd6840

---
+++ Gatsby/hello-world/src/pages/about-css-modules.js
@@ -0,0 +1,30 @@
+import React from "react"
+import styles from "./about-css-modules.module.css"
+import Container from "../components/container"
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
+
+export default () => (
+  <Container>
+    <h1>About CSS Modules</h1>
+    <p>CSS Modules are cool</p>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+  </Container>
+)
