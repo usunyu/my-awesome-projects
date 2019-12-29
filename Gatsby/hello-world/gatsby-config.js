81f2977a6f8101960519d99a9c8825fbbcc9fc7e
Yu Sun <usunyu@gmail.com>
2019-12-28 17:51:56 -0800

    [Gatsby] Source Plugins
    Former-commit-id: 6cc8e1e426d53a00e876e21d606cc6bc3021bf35

--- Gatsby/hello-world/gatsby-config.js
+++ Gatsby/hello-world/gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
     title: `Pandas Eating Lots`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
