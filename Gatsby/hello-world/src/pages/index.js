e7f53e9db0489e73b8d0386983f0f2d076946050
Yu Sun <usunyu@gmail.com>
2019-12-30 11:33:48 -0800

    [Gatsby] Programmatically create pages from data
    Former-commit-id: 529939b5a8f316832a18183dc301ce4b3a1eb043

--- Gatsby/hello-world/src/pages/index.js
+++ Gatsby/hello-world/src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { graphql } from "gatsby"
 import { css } from "@emotion/core"
+import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
 export default ({ data }) => {
-  console.log(data)
   return (
     <Layout>
       <div>
@@ -20,21 +19,29 @@ export default ({ data }) => {
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <h3
+            <Link
+              to={node.fields.slug}
               css={css`
-                margin-bottom: ${rhythm(1 / 4)};
+                text-decoration: none;
+                color: inherit;
               `}
             >
-              {node.frontmatter.title}{" "}
-              <span
+              <h3
                 css={css`
-                  color: #bbb;
+                  margin-bottom: ${rhythm(1 / 4)};
                 `}
               >
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p>{node.excerpt}</p>
+                {node.frontmatter.title}{" "}
+                <span
+                  css={css`
+                    color: #bbb;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
           </div>
         ))}
       </div>
@@ -52,6 +59,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
