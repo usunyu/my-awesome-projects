8297c1341bb140fe0f8eb397a27ae41e34779c40
Yu Sun <usunyu@gmail.com>
2019-12-31 14:07:57 -0800

    [Gatsby] Preparing a Site to Go Live
    Former-commit-id: 70dc61ce847f8c11b4bef31f9f734563320230a1

--- Gatsby/hello-world/gatsby-config.js
+++ Gatsby/hello-world/gatsby-config.js
@@ -7,6 +7,8 @@
 module.exports = {
   siteMetadata: {
     title: `Pandas Eating Lots`,
+    description: `A simple description about pandas eating lots...`,
+    author: `usunyu`,
   },
   plugins: [
     {
@@ -24,5 +26,21 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
   ],
 }
--- Gatsby/hello-world/src/templates/blog-post.js
+++ Gatsby/hello-world/src/templates/blog-post.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
