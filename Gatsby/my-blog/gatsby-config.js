21bd40f9e8422c1c387e63ae47495a9675b0d317
Yu Sun <usunyu@gmail.com>
2020-01-01 17:06:19 -0800

    [Gatsby] Using a Theme
    Former-commit-id: 55fa28f5831cb48e83b9751ed474cfb452956d4c

---
+++ Gatsby/my-blog/gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: "My Blog",
+    author: "Amberley Romo",
+    description: "A collection of my thoughts and writings.",
+    siteUrl: "https://amberley.blog/",
+    social: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/amber1ey",
+      },
+      {
+        name: "github",
+        url: "https://github.com/amberleyromo",
+      },
+    ],
+  },
+}
---
+++ Gatsby/my-blog/src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+// import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    This is my updated bio.
+    <br />
+    It's shadowing the content from the theme.
+  </Fragment>
+)
