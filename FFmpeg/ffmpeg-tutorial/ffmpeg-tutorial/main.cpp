d54bb9d018bbf23e1b4917c38a7cc438bfb5f3bd
Yu Sun <usunyu@gmail.com>
2020-05-17 02:05:52 -0700

    [FFmpeg] AAC音频码流解析
    Former-commit-id: d6c40afd4c63234e550d9fdff8f77aa2135ebbcd

--- FFmpeg/ffmpeg-tutorial/ffmpeg-tutorial/main.cpp
+++ FFmpeg/ffmpeg-tutorial/ffmpeg-tutorial/main.cpp
@@ -41,6 +41,9 @@
 
 #include <iostream>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // (1) 分离YUV420P像素数据中的Y、U、V分量
 /**
@@ -1107,9 +1110,129 @@ int simplest_h264_parser(char *url){
 	return 0;
 }
 
+/*
+ * 视音频数据处理入门：AAC音频码流解析
+ * https://blog.csdn.net/leixiaohua1020/article/details/50535042
+ */
+
+int getADTSframe(unsigned char* buffer, int buf_size, unsigned char* data ,int* data_size){
+	int size = 0;
+ 
+	if(!buffer || !data || !data_size ){
+		return -1;
+	}
+ 
+	while(1){
+		if(buf_size  < 7 ){
+			return -1;
+		}
+		//Sync words
+		if((buffer[0] == 0xff) && ((buffer[1] & 0xf0) == 0xf0) ){
+			size |= ((buffer[3] & 0x03) <<11);     //high 2 bit
+			size |= buffer[4]<<3;                //middle 8 bit
+			size |= ((buffer[5] & 0xe0)>>5);        //low 3bit
+			break;
+		}
+		--buf_size;
+		++buffer;
+	}
+ 
+	if(buf_size < size){
+		return 1;
+	}
+ 
+	memcpy(data, buffer, size);
+	*data_size = size;
+ 
+	return 0;
+}
+ 
+int simplest_aac_parser(char *url)
+{
+	int data_size = 0;
+	int size = 0;
+	int cnt=0;
+	int offset=0;
+ 
+	//FILE *myout=fopen("output_log.txt","wb+");
+	FILE *myout=stdout;
+ 
+	unsigned char *aacframe=(unsigned char *)malloc(1024*5);
+	unsigned char *aacbuffer=(unsigned char *)malloc(1024*1024);
+ 
+	FILE *ifile = fopen(url, "rb");
+	if(!ifile){
+		printf("Open file error");
+		return -1;
+	}
+ 
+	printf("-----+- ADTS Frame Table -+------+\n");
+	printf(" NUM | Profile | Frequency| Size |\n");
+	printf("-----+---------+----------+------+\n");
+ 
+	while(!feof(ifile)){
+		data_size = fread(aacbuffer+offset, 1, 1024*1024-offset, ifile);
+		unsigned char* input_data = aacbuffer;
+ 
+		while(1)
+		{
+			int ret=getADTSframe(input_data, data_size, aacframe, &size);
+			if(ret==-1){
+				break;
+			}else if(ret==1){
+				memcpy(aacbuffer,input_data,data_size);
+				offset=data_size;
+				break;
+			}
+ 
+			char profile_str[10]={0};
+			char frequence_str[10]={0};
+ 
+			unsigned char profile=aacframe[2]&0xC0;
+			profile=profile>>6;
+			switch(profile){
+			case 0: sprintf(profile_str,"Main");break;
+			case 1: sprintf(profile_str,"LC");break;
+			case 2: sprintf(profile_str,"SSR");break;
+			default:sprintf(profile_str,"unknown");break;
+			}
+ 
+			unsigned char sampling_frequency_index=aacframe[2]&0x3C;
+			sampling_frequency_index=sampling_frequency_index>>2;
+			switch(sampling_frequency_index){
+			case 0: sprintf(frequence_str,"96000Hz");break;
+			case 1: sprintf(frequence_str,"88200Hz");break;
+			case 2: sprintf(frequence_str,"64000Hz");break;
+			case 3: sprintf(frequence_str,"48000Hz");break;
+			case 4: sprintf(frequence_str,"44100Hz");break;
+			case 5: sprintf(frequence_str,"32000Hz");break;
+			case 6: sprintf(frequence_str,"24000Hz");break;
+			case 7: sprintf(frequence_str,"22050Hz");break;
+			case 8: sprintf(frequence_str,"16000Hz");break;
+			case 9: sprintf(frequence_str,"12000Hz");break;
+			case 10: sprintf(frequence_str,"11025Hz");break;
+			case 11: sprintf(frequence_str,"8000Hz");break;
+			default:sprintf(frequence_str,"unknown");break;
+			}
+ 
+ 
+			fprintf(myout,"%5d| %8s|  %8s| %5d|\n",cnt,profile_str ,frequence_str,size);
+			data_size -= size;
+			input_data += size;
+			cnt++;
+		}
+ 
+	}
+	fclose(ifile);
+	free(aacbuffer);
+	free(aacframe);
+ 
+	return 0;
+}
+
 int main(int argc, const char * argv[]) {
   
-  std::cout << "视音频数据处理入门：RGB、YUV像素数据处理\n";
+  //std::cout << "视音频数据处理入门：RGB、YUV像素数据处理\n";
   //simplest_yuv420_split("lena_256x256_yuv420p.yuv",256,256,1);
   //simplest_yuv444_split("lena_256x256_yuv444p.yuv",256,256,1);
   //simplest_yuv420_gray("lena_256x256_yuv420p.yuv",256,256,1);
@@ -1121,18 +1244,21 @@ int main(int argc, const char * argv[]) {
   //simplest_rgb24_split("cie1931_500x500.rgb", 500, 500,1);
   //simplest_rgb24_to_bmp("lena_256x256_rgb24.rgb",256,256,"output_lena.bmp");
   //simplest_rgb24_to_yuv420("lena_256x256_rgb24.rgb",256,256,1,"output_lena.yuv");
-  simplest_rgb24_colorbar(640, 360,"colorbar_640x360.rgb");
+  //simplest_rgb24_colorbar(640, 360,"colorbar_640x360.rgb");
   
-  std::cout << "视音频数据处理入门：PCM音频采样数据处理\n";
+  //std::cout << "视音频数据处理入门：PCM音频采样数据处理\n";
   //simplest_pcm16le_split("NocturneNo2inEflat_44.1k_s16le.pcm");
   //simplest_pcm16le_halfvolumeleft("NocturneNo2inEflat_44.1k_s16le.pcm");
   //simplest_pcm16le_doublespeed("NocturneNo2inEflat_44.1k_s16le.pcm");
   //simplest_pcm16le_to_pcm8("NocturneNo2inEflat_44.1k_s16le.pcm");
   //simplest_pcm16le_cut_singlechannel("drum.pcm",2360,120);
-  simplest_pcm16le_to_wave("NocturneNo2inEflat_44.1k_s16le.pcm",2,44100,"output_nocturne.wav");
+  //simplest_pcm16le_to_wave("NocturneNo2inEflat_44.1k_s16le.pcm",2,44100,"output_nocturne.wav");
   
-  std::cout << "视音频数据处理入门：H.264视频码流解析\n";
-  simplest_h264_parser("sintel.h264");
+  //std::cout << "视音频数据处理入门：H.264视频码流解析\n";
+  //simplest_h264_parser("sintel.h264");
+  
+  std::cout << "视音频数据处理入门：AAC音频码流解析\n";
+  simplest_aac_parser("nocturne.aac");
   
   return 0;
 }
