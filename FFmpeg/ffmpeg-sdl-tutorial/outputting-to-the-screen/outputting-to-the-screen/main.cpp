eecfdb10df565f927a6b6128c1d4852b8f34c81e
Yu Sun <usunyu@gmail.com>
2020-06-08 21:14:05 -0700

    [FFmpeg SDL] Outputting to the Screen
    Former-commit-id: 5c633bde59b1d27225de1f0bcf2966d7fc1874d1

--- FFmpeg/ffmpeg-sdl-tutorial/outputting-to-the-screen/outputting-to-the-screen/main.cpp
+++ FFmpeg/ffmpeg-sdl-tutorial/outputting-to-the-screen/outputting-to-the-screen/main.cpp
@@ -1,35 +1,32 @@
 //
-//  tutorial01.cpp
-//  ffmpeg-sdl-player
+//  main.cpp
+//  outputting-to-the-screen
 //
-//  Created by Yu Sun on 6/3/20.
+//  Created by Yu Sun on 6/8/20.
 //  Copyright © 2020 usunyu. All rights reserved.
 //
 
-// tutorial01.c
-// Code based on a tutorial by Martin Bohme (boehme@inb.uni-luebeckREMOVETHIS.de)
+// tutorial02.c
+// A pedagogical video player that will stream through every video frame as fast as it can.
+//
+// Code based on FFplay, Copyright (c) 2003 Fabrice Bellard,
+// and a tutorial by Martin Bohme (boehme@inb.uni-luebeckREMOVETHIS.de)
 // Tested on Gentoo, CVS version 5/01/07 compiled with GCC 4.1.1
 // With updates from https://github.com/chelyaev/ffmpeg-tutorial
 // Updates tested on:
-// LAVC 54.59.100, LAVF 54.29.104, LSWS 2.1.101
+// LAVC 54.59.100, LAVF 54.29.104, LSWS 2.1.101, SDL 1.2.15
 // on GCC 4.7.2 in Debian February 2015
-
-// A small sample program that shows how to use libavformat and libavcodec to
-// read video from a file.
 //
 // Use
 //
-// gcc -o tutorial01 tutorial01.c -lavformat -lavcodec -lswscale -lz
-//
-// to build (assuming libavformat and libavcodec are correctly installed
-// your system).
+// gcc -o tutorial02 tutorial02.c -lavformat -lavcodec -lswscale -lz -lm `sdl-config --cflags --libs`
+// to build (assuming libavformat and libavcodec are correctly installed,
+// and assuming you have sdl-config. Please refer to SDL docs for your installation.)
 //
 // Run using
+// tutorial02 myvideofile.mpg
 //
-// tutorial01 myvideofile.mpg
-//
-// to write the first five frames from "myvideofile.mpg" to disk in PPM
-// format.
+// to play the video stream on your screen.
 
 extern "C"
 {
@@ -37,61 +34,55 @@ extern "C"
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
 
+#include <SDL/SDL.h>
+#include <SDL/SDL_thread.h>
+
+//#ifdef __MINGW32__
+#undef main /* Prevents SDL from overriding main() */
+//#endif
+
 #include <stdio.h>
-}
 
 // compatibility with newer API
 #if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(55,28,1)
 #define av_frame_alloc avcodec_alloc_frame
 #define av_frame_free avcodec_free_frame
 #endif
-
-void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame) {
-  FILE *pFile;
-  char szFilename[32];
-  int  y;
-  
-  // Open file
-  sprintf(szFilename, "/Users/sun/Desktop/frame%d.ppm", iFrame);
-  pFile=fopen(szFilename, "wb");
-  if(pFile==NULL)
-    return;
-  
-  // Write header
-  fprintf(pFile, "P6\n%d %d\n255\n", width, height);
-  
-  // Write pixel data
-  for(y=0; y<height; y++)
-    fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
-  
-  // Close file
-  fclose(pFile);
 }
 
+const char* inputVideo = "/Users/sun/Desktop/video.mpg";
+
 int main(int argc, char *argv[]) {
-  // Initalizing these to NULL prevents segfaults!
-  AVFormatContext   *pFormatCtx = NULL;
-  int               i, videoStream;
-  AVCodecContext    *pCodecCtxOrig = NULL;
-  AVCodecContext    *pCodecCtx = NULL;
-  AVCodec           *pCodec = NULL;
-  AVFrame           *pFrame = NULL;
-  AVFrame           *pFrameRGB = NULL;
-  AVPacket          packet;
-  int               frameFinished;
-  int               numBytes;
-  uint8_t           *buffer = NULL;
+  AVFormatContext *pFormatCtx = NULL;
+  int             i, videoStream;
+  AVCodecContext  *pCodecCtxOrig = NULL;
+  AVCodecContext  *pCodecCtx = NULL;
+  AVCodec         *pCodec = NULL;
+  AVFrame         *pFrame = NULL;
+  AVPacket        packet;
+  int             frameFinished;
+  float           aspect_ratio;
   struct SwsContext *sws_ctx = NULL;
   
+  SDL_Overlay     *bmp;
+  SDL_Surface     *screen;
+  SDL_Rect        rect;
+  SDL_Event       event;
+  
   //if(argc < 2) {
-  //  printf("Please provide a movie file\n");
-  //  return -1;
+  //  fprintf(stderr, "Usage: test <file>\n");
+  //  exit(1);
   //}
   // Register all formats and codecs
   av_register_all();
   
+  if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
+    fprintf(stderr, "Could not initialize SDL - %s\n", SDL_GetError());
+    exit(1);
+  }
+  
   // Open video file
-  if(avformat_open_input(&pFormatCtx, "/Users/sun/Desktop/video.mp4", NULL, NULL)!=0)
+  if(avformat_open_input(&pFormatCtx, inputVideo, NULL, NULL)!=0)
     return -1; // Couldn't open file
   
   // Retrieve stream information
@@ -99,7 +90,7 @@ int main(int argc, char *argv[]) {
     return -1; // Couldn't find stream information
   
   // Dump information about file onto standard error
-  av_dump_format(pFormatCtx, 0, "/Users/sun/Desktop/video.mp4", 0);
+  av_dump_format(pFormatCtx, 0, inputVideo, 0);
   
   // Find the first video stream
   videoStream=-1;
@@ -119,6 +110,7 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Unsupported codec!\n");
     return -1; // Codec not found
   }
+  
   // Copy context
   pCodecCtx = avcodec_alloc_context3(pCodec);
   if(avcodec_copy_context(pCodecCtx, pCodecCtxOrig) != 0) {
@@ -133,21 +125,22 @@ int main(int argc, char *argv[]) {
   // Allocate video frame
   pFrame=av_frame_alloc();
   
-  // Allocate an AVFrame structure
-  pFrameRGB=av_frame_alloc();
-  if(pFrameRGB==NULL)
-    return -1;
+  // Make a screen to put our video
+#ifndef __DARWIN__
+  screen = SDL_SetVideoMode(pCodecCtx->width, pCodecCtx->height, 0, 0);
+#else
+  screen = SDL_SetVideoMode(pCodecCtx->width, pCodecCtx->height, 24, 0);
+#endif
+  if(!screen) {
+    fprintf(stderr, "SDL: could not set video mode - exiting\n");
+    exit(1);
+  }
   
-  // Determine required buffer size and allocate buffer
-  numBytes=avpicture_get_size(AV_PIX_FMT_RGB24, pCodecCtx->width,
-                              pCodecCtx->height);
-  buffer=(uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
-  
-  // Assign appropriate parts of buffer to image planes in pFrameRGB
-  // Note that pFrameRGB is an AVFrame, but AVFrame is a superset
-  // of AVPicture
-  avpicture_fill((AVPicture *)pFrameRGB, buffer, AV_PIX_FMT_RGB24,
-                 pCodecCtx->width, pCodecCtx->height);
+  // Allocate a place to put our YUV image on that screen
+  bmp = SDL_CreateYUVOverlay(pCodecCtx->width,
+                             pCodecCtx->height,
+                             SDL_YV12_OVERLAY,
+                             screen);
   
   // initialize SWS context for software scaling
   sws_ctx = sws_getContext(pCodecCtx->width,
@@ -155,12 +148,14 @@ int main(int argc, char *argv[]) {
                            pCodecCtx->pix_fmt,
                            pCodecCtx->width,
                            pCodecCtx->height,
-                           AV_PIX_FMT_RGB24,
+                           AV_PIX_FMT_YUV420P,
                            SWS_BILINEAR,
                            NULL,
                            NULL,
                            NULL
                            );
+  
+  
   
   // Read frames and save first five frames to disk
   i=0;
@@ -172,30 +167,51 @@ int main(int argc, char *argv[]) {
       
       // Did we get a video frame?
       if(frameFinished) {
-        // Convert the image from its native format to RGB
+        SDL_LockYUVOverlay(bmp);
+        
+        AVPicture pict;
+        pict.data[0] = bmp->pixels[0];
+        pict.data[1] = bmp->pixels[2];
+        pict.data[2] = bmp->pixels[1];
+        
+        pict.linesize[0] = bmp->pitches[0];
+        pict.linesize[1] = bmp->pitches[2];
+        pict.linesize[2] = bmp->pitches[1];
+        
+        // Convert the image into YUV format that SDL uses
         sws_scale(sws_ctx, (uint8_t const * const *)pFrame->data,
                   pFrame->linesize, 0, pCodecCtx->height,
-                  pFrameRGB->data, pFrameRGB->linesize);
+                  pict.data, pict.linesize);
         
-        // Save the frame to disk
-        if(++i<=5)
-          SaveFrame(pFrameRGB, pCodecCtx->width, pCodecCtx->height,
-                    i);
+        SDL_UnlockYUVOverlay(bmp);
+        
+        rect.x = 0;
+        rect.y = 0;
+        rect.w = pCodecCtx->width;
+        rect.h = pCodecCtx->height;
+        SDL_DisplayYUVOverlay(bmp, &rect);
+        
       }
     }
     
     // Free the packet that was allocated by av_read_frame
     av_free_packet(&packet);
+    SDL_PollEvent(&event);
+    switch(event.type) {
+      case SDL_QUIT:
+        SDL_Quit();
+        exit(0);
+        break;
+      default:
+        break;
+    }
+    
   }
-  
-  // Free the RGB image
-  av_free(buffer);
-  av_frame_free(&pFrameRGB);
   
   // Free the YUV frame
   av_frame_free(&pFrame);
   
-  // Close the codecs
+  // Close the codec
   avcodec_close(pCodecCtx);
   avcodec_close(pCodecCtxOrig);
   
