1739ba65e62163ef739310547a5d0e6dbe1c7d40
Yu Sun <usunyu@gmail.com>
2021-03-26 18:12:58 -0700

    [IBAPI] Asynchronous Implementation Intro
    Former-commit-id: 8896aa34d559118e0ed152380dc51970026d5694

---
+++ Algorithmic Trading/ib_api/ib_contract_asynch.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+IBAPI - Getting Contract info
+
+@author: Mayank Rasu (http://rasuquant.com/wp/)
+"""
+
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
+import threading
+import time
+
+
+class TradingApp(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self,self)
+        
+    def error(self, reqId, errorCode, errorString):
+        print("Error {} {} {}".format(reqId,errorCode,errorString))
+        
+    def contractDetails(self, reqId, contractDetails):
+        print("redID: {}, contract:{}".format(reqId,contractDetails))
+
+def websocket_con():
+    app.run()
+    
+app = TradingApp()      
+app.connect("127.0.0.1", 7497, clientId=1)
+
+# starting a separate daemon thread to execute the websocket connection
+con_thread = threading.Thread(target=websocket_con, daemon=True)
+con_thread.start()
+time.sleep(1) # some latency added to ensure that the connection is established
+
+#creating object of the Contract class - will be used as a parameter for other function calls
+contract = Contract()
+contract.symbol = "AAPL"
+contract.secType = "STK"
+contract.currency = "USD"
+contract.exchange = "SMART"
+
+app.reqContractDetails(100, contract) # EClient function to request contract details
+time.sleep(5) # some latency added to ensure that the contract details request has been processed
+
+app.disconnect()
