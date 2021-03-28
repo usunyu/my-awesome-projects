88bfc0110dcf7e606f831482099aeb5c0ace6a92
Yu Sun <usunyu@gmail.com>
2021-03-28 11:45:33 -0700

    [IBAPI] IB Order Types
    Former-commit-id: 4a69196b4641803abb31d708544ae1f8d6cc5fbe

---
+++ Algorithmic Trading/ib_api/ib_order_types.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+IBAPI - IB Order Types
+
+@author: Mayank Rasu (http://rasuquant.com/wp/)
+"""
+
+
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
+from ibapi.order import Order
+import threading
+import time
+
+
+class TradingApp(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self,self)
+        
+    def error(self, reqId, errorCode, errorString):
+        print("Error {} {} {}".format(reqId,errorCode,errorString))
+        
+    def nextValidId(self, orderId):
+        super().nextValidId(orderId)
+        self.nextValidOrderId = orderId
+        print("NextValidId:", orderId)
+
+def websocket_con():
+    app.run()
+    
+app = TradingApp()      
+app.connect("127.0.0.1", 7497, clientId=1)
+
+# starting a separate daemon thread to execute the websocket connection
+con_thread = threading.Thread(target=websocket_con, daemon=True)
+con_thread.start()
+time.sleep(1) # some latency added to ensure that the connection is established
+
+#creating object of the Contract class - will be used as a parameter for other function calls
+def usTechStk(symbol,sec_type="STK",currency="USD",exchange="ISLAND"):
+    contract = Contract()
+    contract.symbol = symbol
+    contract.secType = sec_type
+    contract.currency = currency
+    contract.exchange = exchange
+    return contract 
+
+#creating object of the limit order class - will be used as a parameter for other function calls
+def limitOrder(direction,quantity,lmt_price):
+    order = Order()
+    order.action = direction
+    order.orderType = "LMT"
+    order.totalQuantity = quantity
+    order.lmtPrice = lmt_price
+    return order
+
+def marketOrder(direction,quantity):
+    order = Order()
+    order.action = direction
+    order.orderType = "MKT"
+    order.totalQuantity = quantity
+    return order
+
+def stopOrder(direction,quantity,st_price):
+    order = Order()
+    order.action = direction
+    order.orderType = "STP"
+    order.totalQuantity = quantity
+    order.auxPrice = st_price
+    return order
+
+def trailStopOrder(direction,quantity,st_price,tr_step=1):
+    order = Order()
+    order.action = direction
+    order.orderType = "TRAIL"
+    order.totalQuantity = quantity
+    order.auxPrice = tr_step
+    order.trailStopPrice = st_price
+    return order
+
+
+order_id = app.nextValidOrderId
+app.placeOrder(order_id,usTechStk("FB"),marketOrder("BUY",1))
+time.sleep(5)
+
+app.reqIds(-1)
+time.sleep(2)
+order_id = app.nextValidOrderId
+app.placeOrder(order_id,usTechStk("FB"),stopOrder("SELL",1,200))
+time.sleep(5)
+
+app.reqIds(-1)
+time.sleep(2)
+order_id = app.nextValidOrderId
+app.placeOrder(order_id,usTechStk("TSLA"),trailStopOrder("BUY",1,1400,2)) # EClient function to request contract details
+time.sleep(5) # some latency added to ensure that the contract details request has been processed
+
