7ecbda9e33b85ad6cc4b1c6fb8093f32f73cc131
Yu Sun <usunyu@gmail.com>
2021-07-05 20:28:22 -0700

    [Firebase] Functions Tutorial #9 - Creating User Records
    Former-commit-id: ea41adc51171aac7feb70b449954998f2607e29e

--- Firebase/firebase-functions/functions/index.js
+++ Firebase/firebase-functions/functions/index.js
@@ -1,12 +1,18 @@
-
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // auth trigger (new user signup)
 exports.newUserSignup = functions.auth.user().onCreate((user) => {
-  console.log("user created", user.email, user.uid);
+  // console.log("user created", user.email, user.uid);
+  return admin.firestore().collection("users").doc(user.uid).set({
+    email: user.email,
+    upvotedOn: [],
+  });
 });
 
 // auth trigger (user deleted)
 exports.userDeleted = functions.auth.user().onDelete((user) => {
-  console.log("user deleted", user.email, user.uid);
+  // console.log("user deleted", user.email, user.uid);
+  return admin.firestore().collection("users").doc(user.uid).delete();
 });
