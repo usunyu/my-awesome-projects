81c9d0f2067fb6196a71ae10ffde4e15ebcd78e9
Yu Sun <usunyu@gmail.com>
2021-07-05 18:08:20 -0700

    [Firebase] Functions Tutorial #4 - Creating & Deploying a Function
    Former-commit-id: 3c41b9519017650084b57447bfaa4415b2b064c2

--- Firebase/firebase-functions/functions/index.js
+++ Firebase/firebase-functions/functions/index.js
@@ -1,9 +1,14 @@
-const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+
+// http request 1
+exports.randomNumber = functions.https.onRequest((request, response) => {
+  const number = Math.round(Math.random() * 100);
+  console.log(number);
+  response.send(number.toString());
+});
+
+// http request 2
+exports.toMySite = functions.https.onRequest((request, response) => {
+  response.redirect('https://usunyu.com/');
+});
