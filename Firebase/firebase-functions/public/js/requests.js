622a8f50477262c371541415041e0d63538b43cc
Yu Sun <usunyu@gmail.com>
2021-07-07 15:03:26 -0700

    [Firebase] Functions Tutorial #13 - Adding a Vue Instance
    Former-commit-id: 009acefdc5bc28c777313ad2e123f89e6458a08d

--- Firebase/firebase-functions/public/js/requests.js
+++ Firebase/firebase-functions/public/js/requests.js
@@ -1,17 +1,18 @@
-const ref = firebase.firestore().collection('requests');
+var app = new Vue({
+  el: '#app',
+  data: {
+    requests: [],
+  },
+  mounted() {
+    const ref = firebase.firestore().collection('requests');
 
-ref.onSnapshot(snapshot => {
-  // console.log(snapshot);
-
-  let requests = [];
-  snapshot.forEach(doc => {
-    requests.push({ ...doc.data(), id: doc.id });
-  });
-  // console.log(requests);
-
-  let html = '';
-  requests.forEach(request => {
-    html += `<li>${request.text}</li>`
-  });
-  document.querySelector('ul').innerHTML = html;
-});
+    ref.onSnapshot(snapshot => {
+      // console.log(snapshot);
+      let requests = [];
+      snapshot.forEach(doc => {
+        requests.push({ ...doc.data(), id: doc.id });
+      });
+      this.requests = requests;
+    });
+  }
+})
