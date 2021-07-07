96d303f254ae4f3658ce5e87179e3a8f6136497e
Yu Sun <usunyu@gmail.com>
2021-07-07 15:26:37 -0700

    [Firebase] Functions Tutorial #14 - Upvoting Function
    Former-commit-id: 4d7a1938d45cd015ac20ce7e6cc4d7576b20731c

--- Firebase/firebase-functions/functions/index.js
+++ Firebase/firebase-functions/functions/index.js
@@ -34,3 +34,34 @@ exports.addRequest = functions.https.onCall((data, context) => {
     upvotes: 0,
   });
 });
+
+// upvote callable function
+exports.upvote = functions.https.onCall((data, context) => {
+  // check auth state
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+        "unauthenticated",
+        "only authenticated users can upvote");
+  }
+  // get refs for user doc & request doc
+  const user = admin.firestore().collection("users").doc(context.auth.uid);
+  const request = admin.firestore().collection("requests").doc(data.id);
+  return user.get().then((doc) => {
+    // check user hasn't already upvoted the request
+    if (doc.data().upvotedOn.includes(data.id)) {
+      throw new functions.https.HttpsError(
+          "failed-precondition",
+          "you can only upvote something once");
+    }
+    // update user array
+    return user.update({
+      upvotedOn: [...doc.data().upvotedOn, data.id],
+    })
+        .then(() => {
+          // update votes on the request
+          return request.update({
+            upvotes: admin.firestore.FieldValue.increment(1),
+          });
+        });
+  });
+});
